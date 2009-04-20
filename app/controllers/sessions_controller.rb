8acef9aa741913f755fd620c7dccb0bd997c2c8b
Yann Lugrin <yann.lugrin@liquid-concept.ch>
2009-04-20 13:27:35 +0200

    xml response status for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,32 +1,39 @@
 class SessionsController < ApplicationController
   def new
     @user_session = UserSession.new
+
+    respond_to do |format|
+      format.html
+      format.xml
+    end
   end
 
   def create
     @user_session = UserSession.new(params[:user_session])
+
     respond_to do |format|
       if @user_session.save
         format.html do
           flash[:notice] = I18n.t 'flash.notice.login_successfull'
           redirect_back_or_default users_url
         end
-        format.xml { head :login_successfull }
+        format.xml { head :ok }
       else
         format.html { render :action => :new }
-        format.xml { head :login_failed }
+        format.xml { head :unprocessable_entity }
       end
     end
   end
 
   def destroy
     destroy_current_user_session
+
     respond_to do |format|
       format.html do
         flash[:notice] = I18n.t 'flash.notice.logout_successfull'
         redirect_back_or_default new_session_url
       end
-      format.xml { head :logout_successfull }
+      format.xml { head :unprocessable_entity }
     end
   end
 end
