fb05f2b1ce2c800eea4f0bb9acc9893683ce0b8e
Yann Lugrin <yann.lugrin@liquid-concept.ch>
2009-04-28 17:25:25 +0200

    svg mime type

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -4,4 +4,4 @@
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
 
-Mime::Type.register "application/uniboard", :uniboard
+Mime::Type.register "image/svg+xml", :svg
