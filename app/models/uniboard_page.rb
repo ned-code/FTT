de1d9cf0b6d39591668ee0b9d1183fbc9539614e
Yann Lugrin <yann.lugrin@liquid-concept.ch>
2009-05-07 15:48:06 +0200

    s3 connection in page model

--- app/models/uniboard_page.rb
+++ app/models/uniboard_page.rb
@@ -9,10 +9,14 @@ class UniboardPage < ActiveRecord::Base
   validates_format_of :uuid, :with => UUID_FORMAT_REGEX
 
   def url
+    establish_connection!
+
     AWS::S3::S3Object.url_for("documents/#{document.uuid}/#{uuid}.svg", bucket)
   end
 
   def thumbnail_url
+    establish_connection!
+
     AWS::S3::S3Object.url_for("documents/#{document.uuid}/#{uuid}.thumbnail.jpg", bucket)
   end
 
@@ -28,4 +32,23 @@ class UniboardPage < ActiveRecord::Base
   def bucket
     document.bucket
   end
+
+  private
+
+    def s3_config
+      document.class.s3_config
+    end
+
+    def establish_connection!
+      unless AWS::S3::Base.connected?
+        AWS::S3::Base.establish_connection!(
+            :access_key_id     => s3_config['aws_access_key'],
+            :secret_access_key => s3_config['aws_secret_access_key'],
+            :use_ssl           => s3_config['use_ssl'] || true,
+            :persistent        => s3_config['persistent'] || true
+          )
+      end
+
+      AWS::S3::Bucket.create(bucket) unless AWS::S3::Bucket.list.find {|b| b.name == bucket}
+    end
 end
