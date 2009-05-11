6b745b0e5cbd9bfd9f381a06cc38d92247889728
Yann Lugrin <yann.lugrin@liquid-concept.ch>
2009-05-11 14:41:47 +0200

    use libxml for RightAWS

--- config/initializers/myuniboard.rb
+++ config/initializers/myuniboard.rb
@@ -1,3 +1,5 @@
+RightAws::RightAWSParser.xml_lib = 'libxml'
+
 UniboardDocument.config do |config|
   config[:storage] = :s3
   config[:s3] = YAML::load_file(File.join(RAILS_ROOT, 'config', 's3.yml'))[RAILS_ENV] if File.exists?(File.join(RAILS_ROOT, 'config', 's3.yml'))
