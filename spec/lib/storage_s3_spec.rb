80d5dec883bcaf5ef708e88097cc1f0e557005d3
Yann Lugrin <yann.lugrin@liquid-concept.ch>
2009-05-13 12:50:51 +0200

    Refactor UniboardDocument Config to use Struct

--- spec/lib/storage_s3_spec.rb
+++ spec/lib/storage_s3_spec.rb
@@ -2,6 +2,15 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 require 'storage/s3'
 
 class TestStorage
+
+  Configuration = Struct.new(:storage, :storage_config)
+
+  def config
+   @config ||= Configuration.new(
+        :storage => :filesystem,
+        :storage_config => {}
+      )
+  end
 end
 
 describe 'Uniboard Document storage with S3' do
@@ -25,7 +34,9 @@ describe 'Uniboard Document storage with S3' do
   end
 
   it 'Configuration should not raise error with valid configuration Hash' do
-    @test_storage.should_receive(:config).and_return({:s3 => @valid_s3_config})
+    s3_config = @valid_s3_config.dup
+
+    @test_storage.config.should_receive(:storage_config).and_return(s3_config)
 
     @test_storage.s3_config.should be_a_kind_of(Storage::S3::Configuration)
   end
@@ -34,7 +45,7 @@ describe 'Uniboard Document storage with S3' do
     s3_config = @valid_s3_config.dup
     s3_config.delete('access_key_id')
 
-    @test_storage.should_receive(:config).and_return({:s3 => s3_config})
+    @test_storage.config.should_receive(:storage_config).and_return(s3_config)
 
     lambda { @test_storage.s3_config }.should raise_error(ArgumentError, /access_key_id/)
   end
@@ -43,7 +54,7 @@ describe 'Uniboard Document storage with S3' do
     s3_config = @valid_s3_config.dup
     s3_config.delete('secret_access_key')
 
-    @test_storage.should_receive(:config).and_return({:s3 => s3_config})
+    @test_storage.config.should_receive(:storage_config).and_return(s3_config)
 
     lambda { @test_storage.s3_config }.should raise_error(ArgumentError, /secret_access_key/)
   end
@@ -52,7 +63,7 @@ describe 'Uniboard Document storage with S3' do
     s3_config = @valid_s3_config.dup
     s3_config.delete('bucket')
 
-    @test_storage.should_receive(:config).and_return({:s3 => s3_config})
+    @test_storage.config.should_receive(:storage_config).and_return(s3_config)
 
     lambda { @test_storage.s3_config }.should raise_error(ArgumentError, /bucket/)
   end
