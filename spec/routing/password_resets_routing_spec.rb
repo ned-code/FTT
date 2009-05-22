f2a33765f7dc37108b196af5d4666c9b0de37b91
Yann Lugrin <yann.lugrin@liquid-concept.ch>
2009-05-22 17:22:33 +0200

    Routing spec syntax warning

--- spec/routing/password_resets_routing_spec.rb
+++ spec/routing/password_resets_routing_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe PasswordResetsController do
 
-  it { should route :get,     '/session/password_reset/new',    :controller => :password_resets, :action => :new     }
-  it { should route :post,    '/session/password_reset',        :controller => :password_resets, :action => :create  }
+  it { should route(:get,     '/session/password_reset/new',    :controller => :password_resets, :action => :new     )}
+  it { should route(:post,    '/session/password_reset',        :controller => :password_resets, :action => :create  )}
 
 end
--- vendor/plugins/generators/generators/my_controller/templates/routing_spec.rb
+++ vendor/plugins/generators/generators/my_controller/templates/routing_spec.rb
@@ -2,10 +2,10 @@ require File.expand_path(File.dirname(__FILE__) + '<%= '/..' * class_nesting_dep
 
 describe <%= class_name %>Controller do
 
-  it { should route :get,     '/<%= plural_name %>',    :controller => :<%= plural_name %>, :action => :index             }
-  it { should route :post,    '/<%= plural_name %>',    :controller => :<%= plural_name %>, :action => :create            }
-  it { should route :get,     '/<%= plural_name %>/1',  :controller => :<%= plural_name %>, :action => :show,    :id => 1 }
-  it { should route :put,     '/<%= plural_name %>/1',  :controller => :<%= plural_name %>, :action => :update,  :id => 1 }
-  it { should route :delete,  '/<%= plural_name %>/1',  :controller => :<%= plural_name %>, :action => :destroy, :id => 1 }
+  it { should route( :get,    '/<%= plural_name %>',   :controller => :<%= plural_name %>, :action => :index             )}
+  it { should route( :post,   '/<%= plural_name %>',   :controller => :<%= plural_name %>, :action => :create            )}
+  it { should route( :get,    '/<%= plural_name %>/1', :controller => :<%= plural_name %>, :action => :show,    :id => 1 )}
+  it { should route( :put,    '/<%= plural_name %>/1', :controller => :<%= plural_name %>, :action => :update,  :id => 1 )}
+  it { should route( :delete, '/<%= plural_name %>/1', :controller => :<%= plural_name %>, :action => :destroy, :id => 1 )}
 
 end
