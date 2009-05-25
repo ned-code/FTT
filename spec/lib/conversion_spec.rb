f899b1c34dc2cd2bb033df4bd7bb112ba98b000a
Julien Bachmann <julien.bachmann@mnemis.com>
2009-05-25 16:52:53 +0200

    Add pdf background convertion Change fixture base document

--- spec/lib/conversion_spec.rb
+++ spec/lib/conversion_spec.rb
@@ -5,27 +5,52 @@ describe "Uniboard document conversion" do
 
   it "Document conversion should generate corresponding html file" do
     expected_result = File.open(fixture_file("conversion/index.html")).read
-    rdf_stream = File.open(fixture_file("conversion/metadata.rdf"))
-    ub_stream = File.open(fixture_file("conversion/00000000-0000-0000-0000-0000000.ub"))
-    
+    document_zip_path = fixture_file("conversion/document.ubz")
+    Zip::ZipFile.foreach(document_zip_path) do |an_entry|
+      #need to create sub folder if needed
+      if (an_entry.name.rindex("/") != nil)
+        sub_folder = an_entry.name[0..an_entry.name.rindex("/")]
+        if (!File.exist?(File.dirname(document_zip_path) + "/" + sub_folder))
+          FileUtils.mkdir_p(File.dirname(document_zip_path) + "/" + sub_folder)
+        end
+      end
+      an_entry.extract(File.dirname(document_zip_path) + "/" + an_entry.name)
+    end 
+        
+    rdf_stream = File.open(File.dirname(document_zip_path) + "/metadata.rdf")
+    ub_stream = File.open(File.dirname(document_zip_path) + "/00000000-0000-0000-0000-0000000.ub")
+  
     result = HtmlConversion::create_html_document("00000000-0000-0000-0000-0000000", ub_stream, rdf_stream)
+       
 #    File.open(File.join(RAILS_ROOT, 'spec', 'output.html'), 'w') do |file|
 #      file << result
 #    end
     
     result.should == expected_result
     
-  end
+  end 
   
   it "Test SVG page should generate corresponding Test HTML page" do
-    expected_result = File.open(fixture_file("conversion/index.html")).read
-    svg_stream = File.open(fixture_file("conversion/page001.svg"))
+    #expected_result = File.open(fixture_file("conversion/page01.xhtml.html")).read
+    document_zip_path = fixture_file("conversion/document.ubz")
+    Zip::ZipFile.foreach(document_zip_path) do |an_entry|
+      #need to create sub folder if needed
+      if (an_entry.name.rindex("/") != nil)
+        sub_folder = an_entry.name[0..an_entry.name.rindex("/")]
+        if (!File.exist?(File.dirname(document_zip_path) + "/" + sub_folder))
+          FileUtils.mkdir_p(File.dirname(document_zip_path) + "/" + sub_folder)
+        end 
+      end
+      an_entry.extract(File.dirname(document_zip_path) + "/" + an_entry.name)
+    end 
+    svg_stream = File.open(File.dirname(document_zip_path) + "/page001.svg")
     result = HtmlConversion::convert_svg_page_to_html("00000000-0000-0000-0000-0000001", svg_stream)
-#    File.open(File.join(RAILS_ROOT, 'spec', 'outputPage.html'), 'w') do |file|
-#      file << result
-#    end
-   
+       
+    FileUtils.mv(File.dirname(document_zip_path), File.join(RAILS_ROOT, 'spec', 'output_document'))
+    File.open(File.join(RAILS_ROOT, 'spec', 'output_document', 'page001.xhtml'), 'w') do |file|
+      file << result 
+    end  
     true#result.should == expected_result
-            
+                  
   end
 end
