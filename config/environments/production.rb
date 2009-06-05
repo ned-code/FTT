2193af5b77f3deca8065e5171187c59fff702e18
Yann Lugrin <yann.lugrin@liquid-concept.ch>
2009-06-05 14:44:50 +0200

    add domain to email config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,8 @@ config.action_view.cache_template_loading            = true
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
 config.action_mailer.smtp_settings = {
+  :address => 'localhost',
+  :domain => 'uniboard.mnemis.com',
   :enable_starttls_auto => false
 }
 
