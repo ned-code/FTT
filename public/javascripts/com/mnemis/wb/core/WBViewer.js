82deabc25f174ac16c27380632b5ff5e0a0efa66
Julien Bachmann <julien.bachmann@mnemis.com>
2009-06-10 17:39:36 +0200

    add loading message when page is loading

--- public/javascripts/com/mnemis/wb/core/WBViewer.js
+++ public/javascripts/com/mnemis/wb/core/WBViewer.js
@@ -40,13 +40,15 @@ com.mnemis.wb.core.WBViewer.prototype.loadPage = function(pageUrl)
 {
     // remove previous page
     $("#ub_board").remove();
-
+    var loading = $("<h1>Loading...</h1>");
+    $("body").append(loading);
     // load new page
     $.get(pageUrl, null, function(data, textStatus)
         {
             var loadedPageData = $(data);
             var boardElement = loadedPageData.find("#ub_board").get(0);
             var loadedPage = new com.mnemis.wb.model.WBPage(boardElement);
+            loading.remove();
             $("body").append(boardElement);
             WB.application.boardController.setCurrentPage(loadedPage);
         }, "xml");
