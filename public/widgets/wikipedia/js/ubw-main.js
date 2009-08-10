cc41d5a5af68f81fbff82024a584aed9b6ef469e
cemsever <cem.sever@mnemis.com>
2009-08-10 16:36:12 +0200

    new path configuration for wiki widgets

--- public/widgets/wikipedia/js/ubw-main.js
+++ public/widgets/wikipedia/js/ubw-main.js
@@ -249,7 +249,7 @@ var textBoxInput = kword.replace(/ /g,'_');
    
    				}
 
-		$('iframe').attr('src',"http://localhost:8888/wikibot/search?input=" + textBoxInput + "&lang="+ lang + '&mode=' + mode) ; 
+		$('iframe').attr('src',"/wikibot/search?input=" + textBoxInput + "&lang="+ lang + '&mode=' + mode) ; 
 	
 			loadingBox.width($('.ubw-container').width()-80);
 			loadingBox.height($('.ubw-container').height()-75);
