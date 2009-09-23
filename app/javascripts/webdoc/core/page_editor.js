6956cca7661ed1ca11aa30f8455edd4e39a8a1ce
Julien Bachmann <julien.bachmann@mnemis.com>
2009-09-23 23:03:07 +0200

    add sample image action

--- app/javascripts/webdoc/core/page_editor.js
+++ app/javascripts/webdoc/core/page_editor.js
@@ -152,6 +152,14 @@ WebDoc.PageEditor = $.klass({
 
   insertImage: function() {
     console.log("insert image");
+	var newItem = new WebDoc.Item();
+	newItem.data.media_type = "image";
+	newItem.data.page_id = WebDoc.application.pageEditor.currentPage.uuid();
+	newItem.data.data.tag = "img";
+	newItem.data.data.src = "/system/files/11d69920-8a86-012c-72df-002500a8be1c/original/Picture_1.png?1253720740";
+	newItem.data.data.css = { top: "225px", left: "600px", width: "150px", height: "150px"};
+	var newItemView = new WebDoc.ItemView(newItem);
+	newItem.save();
   }
 });
 
