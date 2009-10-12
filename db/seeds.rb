6a1f57a0f8420441cdaab7e97b15be900d43910c
Julien Bachmann <julien.bachmann@mnemis.com>
2009-10-12 14:36:37 +0200

    add 3 default widgets

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,16 @@ User.create(pass_hash.merge(:name => "Julien", :email => "julien.bachmann@mnemis
 User.create(pass_hash.merge(:name => "Yann", :email => "yann.ringgenberg@mnemis.com"))
 User.create(pass_hash.merge(:name => "Stelio", :email => "stelio.tzonis@mnemis.com"))
 User.create(pass_hash.merge(:name => "Zeno", :email => "zeno@jilion.com"))
+
+media = Media.new({:file_file_name => "GoogleMap.wgt.zip", :file_content_type => "application/wgt"})
+media[:uuid] = "2f499bf0-9574-012c-72fc-002500a8be1c"
+media[:type] = "Medias::Widget"
+media.save
+media = Media.new({:file_file_name => "CountDown.wgt.zip", :file_content_type => "application/wgt"})
+media[:uuid] = "2589c210-9574-012c-72fc-002500a8be1c"
+media[:type] = "Medias::Widget"
+media.save
+media = Media.new({:file_file_name => "VideoPicker.wgt.zip", :file_content_type => "application/wgt"})
+media[:uuid] = "7f096ba0-994e-012c-72ff-002500a8be1c"
+media[:type] = "Medias::Widget"
+media.save
