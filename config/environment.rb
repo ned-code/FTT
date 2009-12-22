9fa9a7d8b0cce3e4d07885eb8d55daada300c30d
Julien Bachmann <julien.bachmann@mnemis.com>
2009-12-22 17:26:11 +0100

    set acl9 s plugin because it causes a bug in type column when using inheritence

--- config/environment.rb
+++ config/environment.rb
@@ -29,7 +29,6 @@ Rails::Initializer.run do |config|
   config.gem 'hpricot'
   config.gem 'json'
   config.gem 'xmpp4r'
-  config.gem 'acl9'
   config.gem 'carrierwave'
   config.gem 'mini_magick'
   config.gem 'right_aws'
