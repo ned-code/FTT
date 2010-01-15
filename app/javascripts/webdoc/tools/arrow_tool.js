db2ff29275744c24380cabfb7a5a0f4407e6d988
Julien Bachmann <julien.bachmann@mnemis.com>
2010-01-15 16:29:07 +0100

    correct item hilight

--- app/javascripts/webdoc/tools/arrow_tool.js
+++ app/javascripts/webdoc/tools/arrow_tool.js
@@ -74,6 +74,7 @@ WebDoc.ArrowTool = $.klass(WebDoc.Tool, {
   
   mouseOver: function(e) {
     var target = $(e.target);
+    $(".item_layer").css("opacity", 0);
     if (target.hasClass("item_layer") && !this.disableHiLight) {
       target.stop().animate({ opacity: 0.8}, { duration: 100});
     }
