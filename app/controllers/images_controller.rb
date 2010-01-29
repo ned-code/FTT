4916a4e96faa586b396ddab3fb9cb12faf3db825
David Matthey <david.matthey@mnemis.com>
2010-01-29 15:54:00 +0100

    Cleaned image controlled

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -34,8 +34,6 @@ class ImagesController < ApplicationController
   
   # POST /images
   def create
-    sleep 1 #TODO REMOVE THIS
-    
     @image = current_user.images.build(params[:image])
     @image.uuid = params[:image][:uuid]
     
