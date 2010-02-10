876c6efaa292270a7f40958db7775393aef7e6a3
David Matthey <david.matthey@mnemis.com>
2010-02-10 17:01:39 +0100

    Fixed widget manager was broken by last commit

--- app/javascripts/webdoc/core/widget_manager.js
+++ app/javascripts/webdoc/core/widget_manager.js
@@ -6,8 +6,8 @@ WebDoc.WidgetManager = $.klass(
 {  
   initialize: function(document)    
   {
-    MTools.ServerManager.getRecords(WebDoc.Widget, null, this._assignYoutubeWidget.pBind(this), { ajaxParams: { system_widget_name: 'youtube'}});
-    MTools.ServerManager.getRecords(WebDoc.Widget, null, this._assignVimeoWidget.pBind(this), { ajaxParams: { system_widget_name: 'vimeo'}});
+    MTools.ServerManager.getRecords(WebDoc.Widget, 'youtube', this._assignYoutubeWidget.pBind(this));
+    MTools.ServerManager.getRecords(WebDoc.Widget, 'vimeo', this._assignVimeoWidget.pBind(this));
   },
 
   getVimeoWidget: function() {
