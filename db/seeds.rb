2d2f7f42b6320cfeb7191960fcef1b66fa4deaf8
David Matthey <david.matthey@mnemis.com>
2010-02-18 16:22:28 +0100

    Fixed an oversight if category already existing

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,6 +30,8 @@ User.find_by_email("cedric.michelet@icare.info").has_role!("admin")
 # Default categories creation
 ["Activism & Non Profits", "Art", "Books", "Business", "Education", "Entertainment", "Everyday life", "Health & Medicine", "History", "Movies", "News", 
 "Travel", "Products", "Photos", "Politics", "Spiritual", "Sports", "Technology", "Travel"].each do |category_name|
-  Category.create(:name => category_name)
-  puts "Created category: #{category_name}"
+  unless Category.find_by_name(category_name)
+    Category.create(:name => category_name)
+    puts "Created category: #{category_name}"
+  end
 end
