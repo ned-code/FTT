50a1211205a6199f0c6ee3e5546422c0cafc7691
Julien Bachmann <julien.bachmann@mnemis.com>
2010-02-22 09:30:17 +0100

    add admin and guest user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,18 @@
+users = [
+  {:name => 'Admin', :email => "admin@mnemis.com", :pass => 'pass'},
+  {:name => 'Guest',   :email => "guest@mnemis.com", :pass => 'pass'},
+]
+
+users.each do |user|
+  unless User.find_by_name(user[:name])
+    pass_hash = { :password => user[:pass], :password_confirmation => user[:pass]}
+    User.create(pass_hash.merge(:name => user[:name], :email => user[:email]))
+    puts "Created user: #{user[:name]}"
+  end
+end
+
+User.find_by_email("admin@mnemis.com").has_role!("admin")
+User.find_by_email("guest@mnemis.com").has_role!("admin")
 
 # Default categories creation
 ["Activism & Non Profits", "Art", "Books", "Business", "Education", "Entertainment", "Everyday life", "Health & Medicine", "History", "Movies", "News", 
