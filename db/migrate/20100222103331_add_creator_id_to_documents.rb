43395fc6d8cd3ed09ee2450ecd70282c713f3514
David Matthey <david.matthey@mnemis.com>
2010-02-25 09:29:16 +0100

    Moved roles migration to migration file

--- db/migrate/20100222103331_add_creator_id_to_documents.rb
+++ db/migrate/20100222103331_add_creator_id_to_documents.rb
@@ -13,6 +13,18 @@ class AddCreatorIdToDocuments < ActiveRecord::Migration
         puts "Error adding creator to document #{document.id} - #{document.title} - #{e}"
       end
     end
+    
+    # Update roles
+    Role.all(:conditions => { :name => 'owner' }).each do |role|
+      count = Role.all(:conditions => { :name => 'editor', :authorizable_type => role.authorizable_type, :authorizable_id => role.authorizable_id }).count
+      if count > 0 # editor on same document already exists, so delete current record
+        role.destroy
+      else
+        role.name = "editor"
+        role.save
+      end
+    end
+    puts "Roles owner updated to editor"
   end
   
   def self.down
