416af51f2f209b4b7420d22f5170bb11ddade99a
Cédric Michelet <cedric.michelet@icare.info>
2010-02-25 10:40:47 +0100

    Route + action for ajax proxy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   SprocketsApplication.routes(map)
   map.connect 'proxy/resolve', :controller => 'proxy', :action => 'resolve', :conditions => { :method => :get }
+  map.connect 'proxy/get', :controller => 'proxy', :action => 'get', :conditions => { :method => :get }
   
   map.root :controller => 'home', :action => :show
   
