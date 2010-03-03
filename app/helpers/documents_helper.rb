aa6c4ac52b8d4e3e5a17a8419e3e5a1979839f57
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2010-03-03 15:28:44 +0100

    Assured edit_mode? return a boolean

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,7 +1,7 @@
 module DocumentsHelper
   
   def edit_mode?
-    current_user && (current_user.has_role?('admin') || current_user.has_role?("editor", @document))
+    current_user.present? && (current_user.has_role?('admin') || current_user.has_role?("editor", @document))
   end
   
 end
