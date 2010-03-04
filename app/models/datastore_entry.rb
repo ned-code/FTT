91f002b9f28068aaefa766fb1c9578e50cb79a3d
Cédric Michelet <cedric.michelet@icare.info>
2010-03-04 16:55:10 +0100

    refactoring: move code from controller to model, remove useless code

--- app/models/datastore_entry.rb
+++ app/models/datastore_entry.rb
@@ -60,10 +60,24 @@ class DatastoreEntry < ActiveRecord::Base
     #  end
   end
   
+  #return all datastore entries for the current user
+  #and linked to items, medias, pages and documents info 
+  def self.all_for_user_with_extra_data(user_id)
+    @join = "INNER JOIN items ON datastore_entries.widget_uuid=items.uuid"
+    @join += " INNER JOIN medias ON items.media_id=medias.id"
+    @join += " INNER JOIN pages ON items.page_id=pages.id"
+    @join += " INNER JOIN documents ON pages.document_id=documents.id "
+    @select = "documents.title,documents.uuid"
+    @select += ",items.id as i_id, medias.title as m_title, pages.position as p_position, pages.uuid as p_uuid"
+    @select += ",datastore_entries.ds_key as de_key,datastore_entries.ds_value as de_value,datastore_entries.updated_at as de_updated_at"
+    @order = "documents.title, documents.id,medias.title,datastore_entries.updated_at"
+
+    return DatastoreEntry.find(:all,:select => @select, :joins=>@join, :order=>@order, :conditions => {:user_id => user_id})
+  end
+  
   # ====================
   # = Instance Methods =
   # ====================
-  
 end
 
 # == Schema Information
