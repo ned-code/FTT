b06f006384dfbf958aae9b1af3e3545db8a7d10a
Julien Bachmann <julien.bachmann@mnemis.com>
2010-03-11 14:43:37 +0100

    refactor a little bit datastores back-end

--- app/models/datastore_entry.rb
+++ app/models/datastore_entry.rb
@@ -4,60 +4,63 @@ class DatastoreEntry < ActiveRecord::Base
   # ================
   belongs_to :widget, :class_name => "Medias::Widget"
   belongs_to :user
-    
+  
   # ===============
   # = Validations =
   # ===============
-  
+  validates_presence_of :ds_key
+  validates_presence_of :widget_uuid
+
   # =================
   # = Class Methods =
   # =================
-  def self.get_document_of_widget(widget_uuid)
-    #get widget
-    if w = Item.find_by_uuid(widget_uuid)
-      #get page
-      if p = w.page
-        return p.document.uuid
+  def self.createOrUpdate(datastore_id, key, value, is_unique)
+    key_to_save = nil
+    #check if the key already exists
+    if(is_unique)
+      existing_key = DatastoreEntry.find(:first,:conditions => {:ds_key => key, :widget_uuid => datastore_id})
+      if (existing_key && (existing_key.user_id == nil || exist_key.user_id == current_user.id))
+        key_to_save = existing_key
+      else
+        key_to_save = DatastoreEntry.new
+      end
+    else #record is not unique
+      #is user connected
+      if(current_user)
+        #has already a record for itself
+        key_to_save = DatastoreEntry.find(:first,:conditions => {:ds_key => key, :widget_uuid => datastore_id, :user_id => current_user.id})
+        unless key_to_save 
+          key_to_save = DatastoreEntry.new
+        end
+      else
+        #TODO manage anonymous user
+        key_to_save = DatastoreEntry.new
       end
     end
- 
-    return nil
+    
+    if(key_to_save)
+      key_to_save.ds_key = key
+      key_to_save.ds_value = value
+      key_to_save.widget_uuid = datastore_id
+      if (current_user)
+        key_to_save.user_id = current_user.id
+      else
+        #TODO manage anonymous user
+      end    
+      key_to_save.save
+    end    
   end
   
-  def self.can_user_edit_document(document_uuid, user_id)
-    #todo
-    if(user_id == nil)
-      return false
+  def self.getAllDatastoreEntries(datastore_id, key, only_current_user)
+    if(only_current_user)
+      if (current_user)
+        DatastoreEntry.find(:all, :conditions => {:ds_key => key, :widget_uuid => datastore_id, :user_id => current_user.id})
+      else
+        #TODO need to manage anonymous user
+      end
+    else
+      DatastoreEntry.find(:all, :conditions => {:ds_key => key, :widget_uuid => datastore_id})
     end
-
-    return true
-  end
-  
-  def self.can_user_read_document(document_uuid, user_id)
-    #todo
-    if(user_id == nil)
-      return false
-    end
-    
-    return true
-    
-    #if(widget_uuid != nil) #has user read right on document containing widget
-    #  hasRight = true
-    #  #get widget
-    #  if(w = Item.find_by_uuid(widget_uuid))
-    #    #get page
-    #    if(p = w.page)
-    #      #check if has right
-    #      if(r = Role.find(:all, :conditions => {}))
-    #      else
-    #        hasRight = false
-    #      end
-    #    else
-    #      hasRight = false
-    #    end
-    #  else
-    #    hasRight = false
-    #  end
   end
   
   #return all datastore entries for the current user
@@ -71,13 +74,18 @@ class DatastoreEntry < ActiveRecord::Base
     @select += ",items.id as i_id, medias.title as m_title, pages.position as p_position, pages.uuid as p_uuid"
     @select += ",datastore_entries.id as de_id,datastore_entries.ds_key as de_key,datastore_entries.ds_value as de_value,datastore_entries.updated_at as de_updated_at"
     @order = "documents.title, documents.id,medias.title,datastore_entries.updated_at"
-
+    
     return DatastoreEntry.find(:all,:select => @select, :joins=>@join, :order=>@order, :conditions => {:user_id => user_id})
   end
   
   # ====================
   # = Instance Methods =
   # ====================
+  
+  def email
+    user.email
+  end
+  
 end
 
 
@@ -93,4 +101,3 @@ end
 #  created_at  :datetime
 #  updated_at  :datetime
 #
-
