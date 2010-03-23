9aa6372efd2eef28543b141d28f7644530b717f5
Stephen Band <stephen.band@mnemis.com>
2010-03-23 12:41:08 +0100

    forms (all of them, I think) converted to form_for (with error handling)

--- lib/custom_form_builder.rb
+++ lib/custom_form_builder.rb
@@ -28,7 +28,7 @@ class CustomFormBuilder < ActionView::Helpers::FormBuilder
       
       args.last.is_a?(Hash) && args.last.merge!(:id => "#{ActionController::RecordIdentifier.singular_class_name(@object)}_#{field_name}")
       
-      super(field_name, *args) + (inline_errors.nil? ? '' : "<p class='inline_errors'>This field #{inline_errors}</p>")
+      super(field_name, *args) + (inline_errors.nil? ? '' : "<span class='error-message'>This field #{inline_errors}</>")
       # super
     end
   end
