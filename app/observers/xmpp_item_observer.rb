6fcafc3697b5329e9eeb2be857bf84e214b25139
Julien Bachmann <julien.bachmann@mnemis.com>
2010-03-23 10:19:48 +0100

    correct notifiaction on update

--- app/observers/xmpp_item_observer.rb
+++ app/observers/xmpp_item_observer.rb
@@ -2,8 +2,8 @@ class XmppItemObserver < ActiveRecord::Observer
   observe :item
 
   def after_update(item)
-    #TODO find another solution. Currently t is the only solution I found to disable notifixation when 
-    # item is created in the same ransaction then its containing page.
+    #TODO find another solution. Currently it is the only solution I found to disable notification when 
+    # item is created in the same transaction then its containing page.
     if (item.must_notify)    
       message = { :source => xmpp_client_id, :item =>  item.attributes }
       XmppNotification.xmpp_notify(message.to_json, item.page.document.uuid)
@@ -11,8 +11,8 @@ class XmppItemObserver < ActiveRecord::Observer
   end
 
   def after_create(item)
-    #TODO find another solution. Currently t is the only solution I found to disable notifixation when 
-    # item is created in the same ransaction then its containing page.    
+    #TODO find another solution. Currently t is the only solution I found to disable notification when 
+    # item is created in the same transaction then its containing page.    
     if (item.must_notify)
       message = { :source => xmpp_client_id, :item =>  item.attributes }
       if (item.page)
