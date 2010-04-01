064d55a40b50b1d1be00772478913bd18c0cbca7
Stephen Band <stephen.band@mnemis.com>
2010-04-01 10:41:54 +0200

    Updates truncate

--- public/javascripts/jquery.truncate.js
+++ public/javascripts/jquery.truncate.js
@@ -3,7 +3,7 @@
 // 0.5
 // Stephen Band
 // 
-// Truncates the html of a node so that the node is no greater than one line high
+// Truncates the html of a node so that the node is no higher than content in options.match
 // Options:
 // marker					- character or jQuery object placed at end of truncation.
 // match 					- content to match in height, typically smething like '<br /><br />'.
@@ -19,8 +19,7 @@
 				marker: '\u2026',
 				truncateByWord: false
 			},
-			// Uses a zero width space for testing
-			testNode = jQuery('<span/>').html('\u200B');
+			testNode = jQuery('<span/>').html(' &nbsp;').css({color: 'red', background: 'green'});
 	
 	function fillNode(node, html, testNode){
 		node
@@ -38,14 +37,15 @@
 		
 		var o = jQuery.extend({}, options, opt);
 		
-		return this.each(function(){
+		return this
+		.each(function(i){
 			var node = jQuery(this),
 					data = node.data('truncate'),
 					html = text || data && data.html || node.html(),
-					match = o.match || data && data.match || '&nbsp;',
+					match = o.match || data && data.match || '\u200B',
 					newHtml = html,
 					length = html.length,
-					targetOffset, normalOffset, currentOffset, x;
+					targetOffset, normalOffset, currentOffset, x, y;
 			
 			if ( !data ) {
 				node.data('truncate', {
@@ -62,7 +62,7 @@
 			
 			// If it's not visible we can't do anything with it
 			if ( !node.is(":visible") ) {
-			 return;
+				return;
 			}
 			
 			// Offset of the node when it has target content.
@@ -83,10 +83,14 @@
 				return;
 			};
 			
+			// x keeps track of the amount we're adding or subtracting from the
+			// length, while y stops us crashing the browser in an infinite loop
+			// by turning falsy when x has had a value of 1 for a couple of turns
 			x = length;
+			y = 1;
 			
 			// Home in.
-			while ( x > 1 || currentOffset.top > targetOffset.top ) {
+			while ( x > 1 || y-- && currentOffset.top > targetOffset.top ) {
 				x = Math.ceil(x/2);
 				
 				length = currentOffset.top > targetOffset.top ?
@@ -98,7 +102,7 @@
 				currentOffset = testNode.offset();
 			}
 			
-			// Lop off letters until we find a space.
+			// If truncating by words, lop off letters until we find a space.
 			if ( o.truncateByWord ) {
 				while ( length-- ) {
 					if ( /\s/.test( html[length+1] ) ) {
