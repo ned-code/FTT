bb1734a508d56aeafb9c51955b00c743d7b98c61
Stephen Band <stephen.band@mnemis.com>
2010-04-01 17:17:58 +0200

    Adds another loading icon with application-style colors. Re-jigs DOM creation for Apps list in library. Basic styles for Apps library.

--- app/javascripts/webdoc/library/apps_library.js
+++ app/javascripts/webdoc/library/apps_library.js
@@ -1,6 +1,7 @@
 /**
  * @author Zeno Crivelli
  * Modified by David Matthey
+ * Modified by Stephen
 **/
 
 //= require <webdoc/model/widget>
@@ -14,13 +15,16 @@ WebDoc.AppsLibrary = $.klass(WebDoc.Library, {
     // Setup details view
     this._setupDetailsView();
 
-    // Observe thumbnails clicks (with event delegation) for all current and future app thumbnails
-    $("#"+libraryId+" .thumbnails ul li a").live("click", function (event) {
-      // widget data are stored in the img element of the app thumbnail
-      var widgetData = $(event.target).parent().find('img').data("data");
-      this.prepareDetailsView(widgetData);
+    // Observe thumbnails clicks with event delegation
+    $("#"+libraryId).delegate(".thumbnails li a", "click", function (e) {
+      
+      // widget data is stored on the thumbnail element
+      var widgetData = $( e.currentTarget ).data("widget");
+      
+      this.prepareDetailsView( widgetData );
       this.showDetailsView.click();
-      event.preventDefault();
+      
+      e.preventDefault();
     }.pBind(this));
 
     // view transition finished (slide in/out)
@@ -31,9 +35,11 @@ WebDoc.AppsLibrary = $.klass(WebDoc.Library, {
       }
     }.pBind(this));
   },
+  
   didClickOnTab: function($super, tab) {
     $super(tab);
   },
+  
   _setupMyApps: function() {
     this.myAppsId = "my_apps";
     
@@ -41,7 +47,10 @@ WebDoc.AppsLibrary = $.klass(WebDoc.Library, {
     this.myAppsContainer = $('#'+this.myAppsId);
     
     // Setup app thumbnails drag n' drop
-    this.myAppsContainer.find(".thumbnails").bind("dragstart", this._prepareThumbDrag.pBind(this));
+    this.myAppsContainer
+    .find(".thumbnails")
+    .delegate("a", "dragstart", this._prepareThumbDrag.pBind(this));
+    
     $(document.body).append(this.buildMediaDragFeedbackElement("apps", "")); // just to preload the icon (so that it'll be immediately available at the first drag)
     
     // Next/Previous page links
@@ -58,6 +67,7 @@ WebDoc.AppsLibrary = $.klass(WebDoc.Library, {
     }.pBind(this)).appendTo(this.paginationWrap).hide();
     this.myAppsContainer.append(this.paginationWrap);
   },
+  
   _setupDetailsView: function() {
     this.detailsViewImg = this.detailsView.find('.single_app img');
     
@@ -83,13 +93,14 @@ WebDoc.AppsLibrary = $.klass(WebDoc.Library, {
       switch (link.attr("id")) {
         case "add_app_to_page_action":
           ddd("add_app_to_page_action");
-          var widgetData = this.detailsAppContainer.data("data");
+          var widgetData = this.detailsAppContainer.data("widget");
           WebDoc.application.boardController.insertWidget(widgetData);
           break;
       }
     }.pBind(this));
     
   },
+  
   prepareDetailsView: function($super, widgetData) {
     $super(widgetData.properties);
     
@@ -98,20 +109,22 @@ WebDoc.AppsLibrary = $.klass(WebDoc.Library, {
     this.detailsView.attr({'class':"view details_view "+properties.type});
     
     // Store the current properties in detailsAppContainer
-    this.detailsAppContainer.data("data", widgetData);
+    this.detailsAppContainer.data("widget", widgetData);
     
     // Image source (+ store the current data in the img element)
-    this.detailsViewImg.attr({'src':widgetData.properties.icon_url}).data("data", widgetData);
+    this.detailsViewImg.attr({'src': widgetData.properties.icon_url}).data("widget", widgetData);
 
     // Title
     var title = "";
-    if (widgetData.title) title = widgetData.title;
-    this.detailsView.find('.app_title').text(title);
+    if (widgetData.title) {
+      title = widgetData.title;
+      this.detailsView.find('.app_title').text(title);
+    }
     
     // Version
     var versionEl = this.detailsView.find('.app_version');
     if (properties.version)
-      versionEl.text(this._getVersionText(properties.version));
+      versionEl.text( properties.version );
     else
       versionEl.text('');
     
@@ -120,17 +133,18 @@ WebDoc.AppsLibrary = $.klass(WebDoc.Library, {
     var descEl = this.detailsView.find('.app_description');
     descEl.text(desc);
   },
-  _prepareThumbDrag: function(event) {
-    // we take parent and then search down the img because safari and firefox have not the same target.
-    // on firefox target is the a tag but in safari target is the img.
-    var draggingItem = $(event.target).parent().find('img');
-    var widgetData = draggingItem.data("data");
-    this._dragStart(event, widgetData);
+
+  _prepareThumbDrag: function(e) {
+    var thumb = $( e.currentTarget );
+    var widgetData = thumb.data("widget");
+    this._dragStart(e, widgetData);
   },
+
   _prepareAppDrag: function(event) {
-    var widgetData = this.detailsAppContainer.data("data");
+    var widgetData = this.detailsAppContainer.data("widget");
     this._dragStart(event, widgetData);
   },
+  
   _loadMyApps: function(pageIncrement) {
     var appsThumbWrap = this.myAppsContainer.find(".thumbnails");
     
@@ -143,17 +157,27 @@ WebDoc.AppsLibrary = $.klass(WebDoc.Library, {
       this.showSpinner(appsThumbWrap);
       
       MTools.ServerManager.getRecords(WebDoc.Widget, null, function(data) {
+        var appsList, noApps;
+        
         if (data.widgets.length === 0) {
-          var noApps = $("<span>").addClass('no_items').text('No Apps');
+          noApps = $("<span>").addClass('no_items').text('No Apps');
           appsThumbWrap.append(appsThumbWrap);
         }
         else {   
-          var myAppsList = $("<ul>");
+          appsList = $("<ul/>", {
+            'class': 'apps-index thumbs index'
+          });
+          
           for (var i = 0; i < data.widgets.length; i++) {
-            myAppsList.append(this._buildThumbnail(data.widgets[i]));
+            appsList.append( this._buildThumbnail(data.widgets[i])[0] );
           }
           
-          appsThumbWrap.append(myAppsList);
+          // Build DOM tree
+          appsThumbWrap.append(
+            appsList
+          )
+          .find('.title')
+          .truncate();
         }
         this._refreshMyAppsPagination(data.pagination);
         appsThumbWrap.data('loaded', true);
@@ -161,30 +185,32 @@ WebDoc.AppsLibrary = $.klass(WebDoc.Library, {
       }.pBind(this), { ajaxParams: { page:this.myAppsPage }});
     }
   },
+  
   _buildThumbnail: function(widget) {
-    var uuid = widget.uuid();
-    var properties = widget.data.properties;
-    var thumb = $("<img>").attr({
-      src : properties.icon_url,
-      alt : ""
-    })
-    .data("data", widget.getData());
+    var uuid = widget.uuid(),
+        properties = widget.data.properties,
+        thumb = $("<a/>", {
+          href: "#"+widget.data.system_name,
+          css: { backgroundImage: "url('" + properties.icon_url + "')" },
+          title: widget.data.title + " ["+properties.version+"]"
+        }),
+        title = $("<span/>", { 'class': "title" }),
+        version = $("<span/>", { 'class': "version" }),
+        li = $("<li/>");
     
-    var titleEl = $("<strong>").addClass("title").text(widget.data.title);
-    var versionEl = $("<p>").addClass("version").text(this._getVersionText(properties.version));
+    thumb.data( "widget", widget.getData() );
+    title.html( widget.data.title );
+    version.html( "v"+properties.version );
     
-    var divWrap = $("<div>");
-    var liWrap = $("<li>");
-    var aWrap = $("<a href='' title=''></a>");
-    divWrap.append(thumb);
-    divWrap.append(titleEl);
-    divWrap.append(versionEl);
-    
-    aWrap.append(divWrap);
-    liWrap.append(aWrap);
-
-    return liWrap;
+    // Construct DOM tree and return it
+    return li.html(
+      thumb.html(
+        title
+        .add(version)
+      )
+    );
   },
+  
   _refreshMyAppsPagination: function(pagination) {
     var hasPagination = pagination.total_pages > 1 ? true : false;
     if (hasPagination) {
@@ -207,8 +233,5 @@ WebDoc.AppsLibrary = $.klass(WebDoc.Library, {
     var mediaDragFeedbackEl = this.buildMediaDragFeedbackElement("app", properties.icon_url);
     $(document.body).append(mediaDragFeedbackEl);
     dt.setDragImage( mediaDragFeedbackEl[0], 65, 45 );
-  },
-  _getVersionText: function(version) {
-    return version != undefined? "Version: "+version : ""
   }
 });
