a5a835ec7cb75560250a80de4bddb8776ae11ff9
Julien Bachmann <julien.bachmann@mnemis.com>
2010-04-07 17:43:19 +0200

    tmp fix upload backgrond image

--- app/javascripts/webdoc/controllers/right_bar_controllers/page_inspector_controller.js
+++ app/javascripts/webdoc/controllers/right_bar_controllers/page_inspector_controller.js
@@ -243,7 +243,6 @@ WebDoc.PageInspectorController = jQuery.klass(WebDoc.RightBarInspectorController
     var options = {  
       success:       this._displayBackgroundImage.pBind(this), 
       type:          "POST",
-      dataType:      "json",
       error: function(response) { ddd("error", response);}
     };
     try{
@@ -258,6 +257,7 @@ WebDoc.PageInspectorController = jQuery.klass(WebDoc.RightBarInspectorController
 
   _displayBackgroundImage: function(responseText, statusText) {
     // Put thumbnail url in the page data so that it can be re-used later
+    responseText = $.evalJSON(responseText);
     var thumbUrl = responseText.image.properties.thumb_url;
     jQuery('#background_image').attr('src', thumbUrl).data('url', responseText.image.properties.url);
     this._changePageBackgroundImageFromThumb();
