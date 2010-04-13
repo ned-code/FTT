f85d9b87c0e20bafe4af7fc1c2bbcac6713ab25d
Stephen Band <stephen.band@mnemis.com>
2010-04-13 19:10:26 +0200

    Enables explore. Moves a bunch of css around. And some other stuff.

--- app/javascripts/webdoc/explore/explore_handler.js
+++ app/javascripts/webdoc/explore/explore_handler.js
@@ -12,14 +12,19 @@ MTools.Application.beforeMain("explore", function() {
     window.location.href = "/explore?main_filter="+jQuery("#wd-main-filter").val()+"&category_filter="+jQuery(this).val();
   });
 
-  jQuery(document).delegate('a[href="#prev-page"]', 'click', function(){
+  jQuery(document)
+  .delegate('a[href="#prev-page"]', 'click', function(e){
     $("#"+$(this).attr('data-webdoc-document-id')).data('object').prevPage();
-  });
-
-
-  jQuery(document).delegate('a[href="#next-page"]', 'click', function(){
+    e.preventDefault();
+  })
+  .delegate('a[href="#next-page"]', 'click', function(e){
     $("#"+$(this).attr('data-webdoc-document-id')).data('object').nextPage();
+    e.preventDefault();
+  })
+  .delegate('.webdoc-viewer-container', 'click', function(e){
+    var id = $(this).attr('data-webdoc-document-id');
+    window.location.href = '/documents/'+id;
   });
-
-
+  
+  jQuery('.webdoc-viewer-title h4').truncate();
 });
