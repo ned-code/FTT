80ef7f945146df15850989fd89464d7910456b11
Stephen Band <stephen.band@mnemis.com>
2010-04-14 14:40:03 +0200

    Cleans out jquery.panel.js

--- public/javascripts/jquery.panel.js
+++ public/javascripts/jquery.panel.js
@@ -120,8 +120,8 @@ jQuery.fn[plug] = function(){
             'a': callHandler
         })
     )
-    .bind('show-head', toggleHead)
-    .bind('show-foot', toggleFoot)
+    .bind('togglehead', toggleHead)
+    .bind('togglefoot', toggleFoot)
 //    .bind('show-screen', function(){
 //        var elem = jQuery(this),
 //            screen = elem.children('.screen').add( elem.find('#board>.screen') ),
@@ -160,95 +160,5 @@ jQuery.fn[plug] = function(){
         });
     });
 };
-
-})(jQuery);
-
-
-
-// text style chooser
-
-(function(jQuery, undefined){
-
-    var state = [  
-          { fontWeight: 'bold', fontStyle: 'normal' },
-          { fontWeight: 'bold', fontStyle: 'italic' },
-          { fontWeight: 'normal', fontStyle: 'italic' },
-          { fontWeight: 'normal', fontStyle: 'normal' }
-        ],
-        i = 0;
-    
-
-    jQuery('.text-style-toggle')
-    .bind('click', function(){
-        jQuery(this).css( state[ i++ % state.length ] );
-        return false;
-    });
-
-})(jQuery);
-
-(function(jQuery, undefined){
-
-    var state = [  
-          { textDecoration: 'underline', color: 'white' },
-          { textDecoration: 'none', color: 'none' }
-        ],
-        i = 0;
-    
-    jQuery('.text-underline-toggle span')
-    .bind('click', function(){
-        jQuery(this).css( state[ i++ % state.length ] );
-        return false;
-    });
-
-})(jQuery);
-
-
-
-// TODO: make this into a ui-scroller plugin
-
-(function(jQuery, undefined){
-
-jQuery().ready(function(){
-
-    var scroller = jQuery(".scroller"),
-        height = scroller.outerHeight(),
-        thumb = scroller.find('.scroller-thumb'),
-        window = scroller.find('.scroller-window'),
-        list = window.find('li a'),
-        first, last, diff, unit, currentIndex;
-    
-    window.find('ul').bind('scroll', function(){
-        
-        height = scroller.outerHeight();
-        
-        first = first || window.find('li:first').position().top;
-        last = last || window.find('li:last').position().top;
-        diff = diff || last - first;
-        unit = diff / list.length;
-        
-        var pos = -( window.find('li:first').position().top - height/2 + unit/2 ),
-            index = Math.floor( pos/unit ),
-            currentNode;
-        
-        index = index < 0 ? 0 : index < list.length ? index : list.length-1;
-        
-        if ( index !== currentIndex ) {
-            
-            // This should be a callback
-            
-            currentIndex = index;
-            
-            currentNode = list.eq( index < 0 ? 0 : index < list.length ? index : list.length-1 );
-            
-            list.removeClass('current');
-            currentNode.addClass('current');
-            
-            thumb.html(currentNode.clone());
-            
-            // Send result to text tool
-            WebDoc.application.textTool.delegate.editorExec('fontName', currentNode.css('fontFamily'));
-        }
-    });
-});
 
 })(jQuery);
