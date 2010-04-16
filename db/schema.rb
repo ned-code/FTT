1fbc7265eaa2c4e9c88af6758b9c6de3b8526b7c
Julien Bachmann <julien.bachmann@mnemis.com>
2010-04-16 11:24:54 +0200

    add missing indexes

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100317104610) do
+ActiveRecord::Schema.define(:version => 20100416082915) do
 
   create_table "categories", :force => true do |t|
     t.string "name", :null => false
@@ -23,6 +23,9 @@ ActiveRecord::Schema.define(:version => 20100317104610) do
     t.datetime "updated_at"
     t.integer  "item_id"
   end
+
+  add_index "datastore_entries", ["item_id"], :name => "index_datastore_entries_on_item_id"
+  add_index "datastore_entries", ["user_id"], :name => "index_datastore_entries_on_user_id"
 
   create_table "datastores", :force => true do |t|
     t.string   "ds_key",                          :null => false
@@ -47,12 +50,20 @@ ActiveRecord::Schema.define(:version => 20100317104610) do
     t.integer  "views_count",               :default => 0
   end
 
+  add_index "documents", ["category_id"], :name => "index_documents_on_category_id"
+  add_index "documents", ["creator_id"], :name => "index_documents_on_creator_id"
+  add_index "documents", ["is_public"], :name => "index_documents_on_is_public"
+  add_index "documents", ["uuid"], :name => "index_documents_on_uuid"
+
   create_table "followships", :force => true do |t|
     t.integer  "follower_id",  :null => false
     t.integer  "following_id", :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "followships", ["follower_id"], :name => "index_followships_on_follower_id"
+  add_index "followships", ["following_id"], :name => "index_followships_on_following_id"
 
   create_table "items", :force => true do |t|
     t.string   "uuid",       :limit => 36
@@ -66,6 +77,7 @@ ActiveRecord::Schema.define(:version => 20100317104610) do
   end
 
   add_index "items", ["page_id"], :name => "index_items_on_page_id"
+  add_index "items", ["uuid"], :name => "index_items_on_uuid"
 
   create_table "medias", :force => true do |t|
     t.string   "uuid",        :limit => 36
@@ -82,6 +94,7 @@ ActiveRecord::Schema.define(:version => 20100317104610) do
 
   add_index "medias", ["system_name"], :name => "index_medias_on_system_name"
   add_index "medias", ["user_id"], :name => "index_medias_on_user_id"
+  add_index "medias", ["uuid"], :name => "index_medias_on_uuid"
 
   create_table "pages", :force => true do |t|
     t.string   "uuid",         :limit => 36
@@ -96,6 +109,7 @@ ActiveRecord::Schema.define(:version => 20100317104610) do
   end
 
   add_index "pages", ["document_id"], :name => "index_pages_on_document_id"
+  add_index "pages", ["uuid"], :name => "index_pages_on_uuid"
 
   create_table "roles", :force => true do |t|
     t.string   "name",              :limit => 40
@@ -164,5 +178,7 @@ ActiveRecord::Schema.define(:version => 20100317104610) do
     t.string   "ip_address"
     t.datetime "created_at"
   end
+
+  add_index "view_counts", ["session_id"], :name => "index_view_counts_on_session_id"
 
 end
