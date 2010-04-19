625a03c3f8bdff3bc371d42d7b45e53c3b691345
Julien Bachmann <julien.bachmann@mnemis.com>
2010-04-19 09:33:26 +0200

    fix #90

--- app/javascripts/webdoc/gui/item_view.js
+++ app/javascripts/webdoc/gui/item_view.js
@@ -79,6 +79,7 @@ WebDoc.ItemView = $.klass({
           case "innerHTML":
           // for compatibility we also check innerHtml like this because old cocument can have this key instead of innerHTML
           case "innerHtml":
+          case "tag":
           case "css":        
           case "preference":
           case "properties":
@@ -126,10 +127,9 @@ WebDoc.ItemView = $.klass({
       this.itemDomNode.css(itemCss);
     }
   },
- 
-  
+
   innerHtmlChanged: function() {
-    if (!WebDoc.application.pageEditor.disableHtml) {    
+    if (!WebDoc.application.pageEditor.disableHtml && this.item.data.data.tag !== "iframe") {    
       this.itemDomNode.html($.string().stripScripts(this.item.data.data.innerHTML));
     }
   },
