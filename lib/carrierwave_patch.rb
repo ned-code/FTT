6e61872ee6ccbfa691fb56937d7a7f9a709f2f03
Noé Froidevaux <noe.froidevaux@webdoc.com>
2010-05-19 13:45:18 +0200

    monkey patch to redefine sanitize method of carrierwave to remove '+' in filename

--- lib/carrierwave_patch.rb
+++ lib/carrierwave_patch.rb
@@ -1,16 +1,12 @@
 # encoding: utf-8
 
-
-# monkey patch to redefine url method
-# in version 0.4.5 of CarrierWave with RightS3, url method don't work,
-# so this patch force to use the version 0.4.3 of this method 
-
 module CarrierWave
   module Storage
     class RightS3 < Abstract
-
       class File
-
+        # redefine url method
+        # in version 0.4.5 of CarrierWave with RightS3, url method don't work,
+        # so this patch force to use the version 0.4.3 of this method
         def url
           if @uploader.s3_cnamed
             ["http://", @uploader.s3_bucket, @path].compact.join('/')
@@ -18,8 +14,19 @@ module CarrierWave
             ["http://#{@uploader.s3_bucket}.s3.amazonaws.com", @path].compact.join('/')
           end
         end
-        
       end
+    end
+  end
+
+  class SanitizedFile
+    # redefine sanitize to remove + in filename
+    def sanitize(name)
+      name = name.gsub("\\", "/") # work-around for IE
+      name = File.basename(name)
+      name = name.gsub(/[^a-zA-Z0-9\.\-_]/,"_")
+      name = "_#{name}" if name =~ /\A\.+\z/
+      name = "unnamed" if name.size == 0
+      return name.downcase
     end
   end
 end
