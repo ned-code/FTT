502a6654527b98067a388adf41e61b0abacc01cc
Julien Bachmann <julien.bachmann@mnemis.com>
2010-05-28 10:26:06 +0200

    test document/featured route

--- spec/routing/documents_routing_spec.rb
+++ spec/routing/documents_routing_spec.rb
@@ -4,6 +4,7 @@ describe DocumentsController do
   
   should_route :get,     '/documents',          :action => :index
   should_route :get,     '/documents/explore',  :action => :explore
+  should_route :get,     '/documents/featured',  :action => :featured
   should_route :get,     '/documents/1',        :action => :show,    :id => 1
   should_route :get,     '/documents/1/edit',   :action => :edit,    :id => 1
   should_route :post,    '/documents',          :action => :create
