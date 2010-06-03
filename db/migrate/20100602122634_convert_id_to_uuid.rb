5d5e4d5b6caa9e7ab3195ca3df2a0e0012536d98
Jonathan Biolaz <jonathan.biolaz@webdoc.com>
2010-06-03 14:21:11 +0200

    update sql in migration id_to_uuid

--- db/migrate/20100602122634_convert_id_to_uuid.rb
+++ db/migrate/20100602122634_convert_id_to_uuid.rb
@@ -170,8 +170,8 @@ class ConvertIdToUuid < ActiveRecord::Migration
       user = User.find(:first, :conditions => { :id => r.user_id })
       role = Role.find(:first, :conditions => { :id => r.role_id })
     
-      execute "UPDATE roles_users SET user_id='#{user.uuid}' where user_id='#{user.id}' AND role_id='#{role.id}'"
-      execute "UPDATE roles_users SET role_id='#{role.uuid}' where user_id='#{user.uuid}' AND role_id='#{role.id}'"
+      execute "UPDATE roles_users SET user_id='#{user.uuid}, role_id='#{role.uuid}' where user_id='#{user.id}' AND role_id='#{role.id}'"
+      #execute "UPDATE roles_users SET role_id='#{role.uuid}' where user_id='#{user.uuid}' AND role_id='#{role.id}'"
     end
     
     #Theme
