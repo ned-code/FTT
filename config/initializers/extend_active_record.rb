51fe614340503d6c33c31a5c364e2e3fd228bab4
Jonathan Biolaz <jonathan.biolaz@webdoc.com>
2010-06-04 15:16:52 +0200

    added created_with_uuid

--- config/initializers/extend_active_record.rb
+++ config/initializers/extend_active_record.rb
@@ -5,4 +5,10 @@ class ActiveRecord::Base
     new_record.uuid = params[:uuid]
     new_record
   end
+  
+  def self.create_with_uuid(params)
+    new_record = self.class_name.constantize.new_with_uuid(params)
+    new_record.save
+    new_record
+  end
 end
