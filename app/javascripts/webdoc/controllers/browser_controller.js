05a69001c83b1b55a5074eb944cc2b73cb4f4bf3
Jonathan Biolaz <jonathan.biolaz@webdoc.com>
2010-06-16 10:54:09 +0200

    added empty controller browser in rails and js

---
+++ app/javascripts/webdoc/controllers/browser_controller.js
@@ -0,0 +1,19 @@
+/**
+ * @author jonathan
+ */
+
+WebDoc.BrowserController = $.klass({
+  initialize: function() {
+		ddd('[BrowserController] initialize');
+  },
+
+	openBrowser: function(){
+		
+	},
+	
+	bringToFront: function(){
+		
+	}
+	
+	
+});
