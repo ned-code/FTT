9d90cee3afe3996061b3a414a0f7ec9247c53486
Julien Bachmann <julien.bachmann@webdoc.com>
2010-06-24 14:19:25 +0200

    fix inner_html migration

--- db/migrate/20100623082134_convert_inner_html.rb
+++ db/migrate/20100623082134_convert_inner_html.rb
@@ -1,12 +1,12 @@
 class ConvertInnerHtml < ActiveRecord::Migration
   def self.up
     Item.all.each do |item|
-      if (item.data[:innerHTML])                
+      if (item.data && item.data[:innerHTML])                
         item.inner_html = item.data[:innerHTML]
         item.data.delete(:innerHTML)
         item.touch_page_active = false
         item.save
-      elsif (item.data[:innerHtml])
+      elsif (item.data && item.data[:innerHtml])
         item.inner_html = item.data[:innerHtml]
         item.data.delete(:innerHtml)
         item.touch_page_active = false
