12eef471326ea3f3bd8662bb547d4f63079fd156
Julien Bachmann <julien.bachmann@webdoc.com>
2010-06-25 12:20:19 +0200

    change external browser position

--- app/javascripts/webdoc/core/webdoc_handlers.js
+++ app/javascripts/webdoc/core/webdoc_handlers.js
@@ -103,7 +103,7 @@ WebDoc.handlers = {
     'redo':                 function(e) { WebDoc.application.undoManager.redo(); },
     'delete':               function(e) { WebDoc.application.boardController.deleteSelection(); },
     
-		'open-browser': 				function(e)	{ WebDoc.application.browserController.openBrowser()}, 
+		'open-browser': 				function(e)	{ WebDoc.application.browserController.openBrowser(e)}, 
     'disable-html':         function(e) { WebDoc.application.pageEditor.toggleDebugMode(); },
     
     'show-thumbs':          function(e) { WebDoc.application.pageBrowserController.showThumbs(e); },
