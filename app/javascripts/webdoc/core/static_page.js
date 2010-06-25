ec74ea962b67e0e2355cfb12d542bfbbb9b7591e
Noé Froidevaux <noe.froidevaux@webdoc.com>
2010-06-25 11:15:03 +0200

    improve static page rendering

--- app/javascripts/webdoc/core/static_page.js
+++ app/javascripts/webdoc/core/static_page.js
@@ -32,41 +32,25 @@ WebDoc.StaticPage = $.klass(WebDoc.Application,{
     WebDoc.application.svgRenderer = new WebDoc.SvgRenderer();
 
     WebDoc.Application.initializeSingletons([WebDoc.ThemeManager], function() {
-      this._loadDocument();
+      this.domNode.html('');
+      this.domNode.addClass('loading');
+      this._createView();
+      this.domNode.removeClass('loading');
     }.pBind(this));
   },
-
-  _loadDocument: function() {
-    ddd("[static page] load document");
-
-    this.domNode.html('');
-    this.domNode.addClass('loading');
-
-    WebDoc.ServerManager.request('/documents/'+this.documentUuid+'/pages/'+this.pageUuid+'.json', function(data) {
-      this._createView(data);
-      this.domNode.removeClass('loading');
-    }.pBind(this), 'GET', this._createAjaxParams());
-  },
-
+  
    _createAjaxParams: function() {
     return {
       secure_token: this.secureToken
     };
   },
 
-  _createView: function(page) {
+  _createView: function() {
     ddd("[static page] create view with documents");
 
-    WebDoc.ServerManager.getRecords(WebDoc.Document, this.documentUuid, function(data) {
-      // new WebDoc.Document();
-      var wdPage = new WebDoc.Page(page, data[0]);
-
-      var div = $('<div/>').attr('class', 'webdoc-viewer-container').attr('css', 'background-color: black; font-size: 133.333%; height: 288px; margin-bottom: 0.8333em; overflow: hidden; position: relative; width: 100%;');
-      this.domNode.append(div);
-
-      var viewer = new WebDoc.WebdocViewer(this.domNode);
-      viewer.loadPage(wdPage);
-    }.pBind(this));
+    var viewer = new WebDoc.WebdocViewer(this.domNode);
+    viewer.load(this.documentUuid);
+    viewer.loadPageId(this.pageUuid);
   }
 
 });
