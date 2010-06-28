4232a3f880d3edd3ab238356243e54f911a2d576
Julien Bachmann <julien.bachmann@webdoc.com>
2010-06-28 09:21:19 +0200

    add some log in xmpp notification module

--- lib/xmpp_notification.rb
+++ lib/xmpp_notification.rb
@@ -3,13 +3,15 @@ require "xmpp4r/pubsub"
 
 class XmppNotification
   @@xmpp_client = Jabber::Client.new('server@webdoc.com')
-  @@xmpp_off = true
+  @@xmpp_off = false
   
   def self.xmpp_create_node(node_name)
     if (!@@xmpp_off)
-      begin        
+      begin  
+        p "************************************ XMPP Get pubsub service"
         service = self.get_pubsub_service("pubsub.webdoc.com") 
         begin
+          p "************************************ Create node"
           service.create_node(node_name, Jabber::PubSub::NodeConfig.new(nil,{ 
                     "pubsub#title" => node_name, 
                     "pubsub#node_type" => "leaf", 
@@ -63,8 +65,10 @@ class XmppNotification
   def self.get_pubsub_service(pubsubjid)
     service = nil
     begin
+      p "************************************ New Serice helper"
       service = Jabber::PubSub::ServiceHelper.new(@@xmpp_client,pubsubjid)
-    rescue Jabber::JabberError => error
+      p "************************************ OK"
+    rescue Exception => error      
       Rails.logger.warn "Error on pubsub service #{error}"
       self.xmpp_connect
       service=Jabber::PubSub::ServiceHelper.new(@@xmpp_client,pubsubjid)
