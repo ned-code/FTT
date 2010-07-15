a59db02a0514bf0405938c40caa6a7676fc1f867
Noé Froidevaux <noe.froidevaux@webdoc.com>
2010-07-15 11:38:15 +0200

    soft delete on discussions and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -12,9 +12,11 @@ class Comment < ActiveRecord::Base
   belongs_to :discussion
 
   named_scope :root_only, :conditions => { :root => true }
+  named_scope :not_deleted, :conditions => ['deleted_at IS ?', nil]
+  named_scope :deleted, :conditions => ['deleted_at IS NOT ?', nil]
 
   before_save :check_if_root
-
+  
   def check_if_root
     if self.discussion.comments.first(:conditions => { :root => true }).present?
       self.root = false
@@ -22,6 +24,11 @@ class Comment < ActiveRecord::Base
       self.root = true
     end
     return true
+  end
+
+  def safe_delete!
+    self.deleted_at = Time.now
+    self.save!
   end
 
 end
