3f618cd6ee601931fc03c67a46bae44bb65fe5a1
Noé Froidevaux <noe.froidevaux@webdoc.com>
2010-07-16 09:55:24 +0200

    update comments

--- app/javascripts/webdoc/gui/discussion_view.js
+++ app/javascripts/webdoc/gui/discussion_view.js
@@ -26,7 +26,7 @@ WebDoc.DiscussionView = $.klass({
     var discussionNode = jQuery('<div/>', { 'style': 'width:16px; height:16px;' });    
     this._icon = jQuery('<img/>', { 'alt':'comment', 'src':'/images/icons/chat_16.png' });
     discussionNode.append(this._icon);
-    discussionNode.append(jQuery('<div/>', { 'style': 'top:3px; width:16px; height:13px; overflow: hidden; position:absolute; font-size: 8px; text-align:center;' }).text(this.discussion.comments.length));
+    discussionNode.append(jQuery('<div/>', { 'style': 'top:3px; width:16px; height:13px; overflow: hidden; position:absolute; font-size: 8px; text-align:center; color: white;' }).text(this.discussion.comments.length));
     // add a div to catch click with a class wd_discussion
     discussionNode.append(jQuery('<div/>', { 'style': 'top:0px; width:16px; height:16px; position:absolute;', 'class': 'wd_discussion' }));
     return discussionNode;
