d3eef49400f5ba4188d877508c6e58e5504c7904
Jonathan Biolaz <jonathan.biolaz@webdoc.com>
2010-07-20 14:06:35 +0200

    added helper to build css from an hash in static page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,9 @@ module ApplicationHelper
   def build_css_string(css_hash)
     css_string = ''
     css_hash.each do |property, value|
+      #convert propertyCapital to property-capital
+      propertyArray = property.split(//)
+      propertyCharArray.each { |c| }
       css_string += "#{property}:#{value};"
     end
     css_string
