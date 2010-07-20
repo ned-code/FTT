b08f67834552c114c39191ccb034cbfb847670e2
Jonathan Biolaz <jonathan.biolaz@webdoc.com>
2010-07-20 12:33:11 +0200

    update static page for google

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 module ApplicationHelper
-
+  def build_css_string(css_hash)
+    css_string = ''
+    css_hash.each do |property, value|
+      css_string += "#{property}:#{value};"
+    end
+    css_string
+  end
 end
