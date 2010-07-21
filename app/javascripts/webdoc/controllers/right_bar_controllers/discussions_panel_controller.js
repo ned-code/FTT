4cb56325ce9abfd779c1e7ee0c712b99999df7c2
Noé Froidevaux <noe.froidevaux@webdoc.com>
2010-07-21 10:25:23 +0200

    add compact and expand mode for discussions

--- app/javascripts/webdoc/controllers/right_bar_controllers/discussions_panel_controller.js
+++ app/javascripts/webdoc/controllers/right_bar_controllers/discussions_panel_controller.js
@@ -31,7 +31,9 @@ WebDoc.DiscussionsPanelController = jQuery.klass(WebDoc.RightBarInspectorControl
     this._discussionsWithListener = [];
     var discussionViews = WebDoc.application.boardController.currentPageView().discussionViews;
     for (var discussionView in discussionViews) {
-      this.discussionsDomNode.append(this.createDiscussionAndFormDomNode(discussionViews[discussionView].discussion));
+      var discussionDomNodeAndForm = this.createDiscussionAndFormDomNode(discussionViews[discussionView].discussion);
+      this.discussionsDomNode.append(discussionDomNodeAndForm);
+      this.compactMode(discussionDomNodeAndForm);
     }
   },
 
@@ -48,8 +50,31 @@ WebDoc.DiscussionsPanelController = jQuery.klass(WebDoc.RightBarInspectorControl
     return discussionDomNode;
   },
 
+  compactMode: function(discussionDomNodeAndForm) {
+    var discussionDomNode = discussionDomNodeAndForm.children('div.wd_discussion_dom_node').first();
+    var formDomNode = discussionDomNodeAndForm.children('div.wd_comment_form_dom_node').first();
+    var divsComments = discussionDomNode.children('div.wd_comment_content_dom_node');
+    if(divsComments.length > 0) {
+      divsComments.each(function() { $(this).hide(); });
+      divsComments.first().show();
+      formDomNode.hide();
+    }
+    else {
+      divsComments.each(function() { $(this).hide(); });
+      formDomNode.show();
+    }
+  },
+
+  expendMode: function (discussionDomNodeAndForm) {
+    var discussionDomNode = discussionDomNodeAndForm.children('div.wd_discussion_dom_node').first();
+    var formDomNode = discussionDomNodeAndForm.children('div.wd_comment_form_dom_node').first();
+    var divsComments = discussionDomNode.children('div.wd_comment_content_dom_node');
+    divsComments.each(function() { $(this).show(); });
+    formDomNode.show();
+  },
+
   createDiscussionDomNode: function(discussion) {
-    var newDiscussionsDomNode = jQuery('<div/>').attr('data-discussion-uuid', discussion.uuid());
+    var newDiscussionsDomNode = jQuery('<div/>').attr('data-discussion-uuid', discussion.uuid()).attr('class', 'wd_discussion_dom_node');
     discussion.addListener(this);
     this._discussionsWithListener.push(discussion);
     for(var i=0; i<discussion.comments.length; i++) {
@@ -61,7 +86,7 @@ WebDoc.DiscussionsPanelController = jQuery.klass(WebDoc.RightBarInspectorControl
   },
 
   createCommentDomNode: function(comment) {
-    var commentDomNode = jQuery('<div/>').attr('data-comment-uuid', comment.uuid()),
+    var commentDomNode = jQuery('<div/>').attr('data-comment-uuid', comment.uuid()).attr('class', 'wd_comment_content_dom_node'),
         firstPart = jQuery('<div/>', { 'style': 'width: 80%; float: left;'}),
         secondPart = jQuery('<div/>', { 'style': 'width: 20%; float: left;'});
     firstPart.append(comment.content().replace(/\n/g, '<br />'));
@@ -84,8 +109,9 @@ WebDoc.DiscussionsPanelController = jQuery.klass(WebDoc.RightBarInspectorControl
 
   createCommentForm: function(discussion) {
     var label = jQuery('<label/>').text('Comment'),
-        commentContent = jQuery('<textarea/>', { name: 'comment', value: 'Your comment' }),
-        form = $('<form/>'),
+        commentContent = jQuery('<textarea/>', { name: 'comment' }),
+        domNode = jQuery('<div/>').attr('class', 'wd_comment_form_dom_node'),
+        form = jQuery('<form/>'),
         button = jQuery('<input/>', { 'type': 'submit', 'value': 'Comment'});
 
     form
@@ -115,18 +141,22 @@ WebDoc.DiscussionsPanelController = jQuery.klass(WebDoc.RightBarInspectorControl
         }
     });
 
-    return form;
+    return domNode.append(form);
   },
 
   selectDiscussion: function(discussion, oldDiscussion) {
     ddd('[DiscussionsPanel] select discussion');
     if (oldDiscussion !== null) {
       var oldDiscussionSelectedDomNode = this.discussionsDomNode.find("div[data-discussion-uuid='"+oldDiscussion.uuid()+"']")[0];
-      jQuery(oldDiscussionSelectedDomNode).parent().removeClass('item_selected');
+      var oldParent = jQuery(oldDiscussionSelectedDomNode).parent();
+      this.compactMode(oldParent);
+      oldParent.removeClass('item_selected');
     }
     var discussionSelectedDomNode = this.discussionsDomNode.find("div[data-discussion-uuid='"+discussion.uuid()+"']")[0];
     if(discussionSelectedDomNode) {
-      jQuery(discussionSelectedDomNode).parent().addClass('item_selected');
+      var parent = jQuery(discussionSelectedDomNode).parent();
+      parent.addClass('item_selected');
+      this.expendMode(parent);
       discussionSelectedDomNode.scrollIntoView(true);
     }
   },
