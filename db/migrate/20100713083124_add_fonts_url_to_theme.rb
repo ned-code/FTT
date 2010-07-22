ae190796fd8d03783809307e01df0fef1669f740
Jonathan Biolaz <jonathan.biolaz@webdoc.com>
2010-07-22 15:20:31 +0200

    update comments style in static page

--- db/migrate/20100713083124_add_fonts_url_to_theme.rb
+++ db/migrate/20100713083124_add_fonts_url_to_theme.rb
@@ -1,9 +1,10 @@
 class AddFontsUrlToTheme < ActiveRecord::Migration
   def self.up
-    add_column :themes, :fonts_url, :string
+    #No used any more
+    # add_column :themes, :fonts_url, :string
   end
 
   def self.down
-    remove_column :themes, :fonts_url
+    # remove_column :themes, :fonts_url
   end
 end
