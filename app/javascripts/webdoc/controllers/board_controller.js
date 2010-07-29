5eae6ed7606c2c06e4cd7997f3f44f0938534c93
Jonathan Biolaz <jonathan.biolaz@webdoc.com>
2010-07-29 15:14:17 +0200

    added an image from flickr that isn't free for use display a message

--- app/javascripts/webdoc/controllers/board_controller.js
+++ app/javascripts/webdoc/controllers/board_controller.js
@@ -661,6 +661,11 @@ WebDoc.BoardController = jQuery.klass({
   },
   
   insertImage: function(imageUrl, position, media_id, title) {
+    ddd('insertImage : ', imageUrl);
+    if(imageUrl.match('spaceball.gif')){
+      alert("Sorry, but this image isn't free for use");
+      return false;
+    }
     var image = document.createElement('img'); /* Preload image in order to have width and height parameters available */
     jQuery(image).bind("load", {position: position, media_id: media_id, title: title }, this._createImageItemAfterLoad); /* WebDoc.Item creation will occur after image load*/
     image.src = imageUrl;
