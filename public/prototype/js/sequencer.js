caae0b9ddcf186743959dc596e6abf7c1da5bcd3
stephband <stephband@webdoc.com>
2010-08-05 14:15:46 +0200

    seq update

--- public/prototype/js/sequencer.js
+++ public/prototype/js/sequencer.js
@@ -2,63 +2,70 @@
 	
 	var debug = (window.console && console.log);
 	
-	var processMap ={
-		'click': DOMEventProcess,
-		'audio': function(){},
-		'sequence': SequenceProcess
+	// Transport --------------------------------------
+	
+	var transport = {
+		play: function(){
+			var now = new Date().getTime();
+			
+			this.starttime = now;
+			
+			console.log('now: ', now);
+			
+			makeProcessQueue(this, now, clock);
+		},
+		latency: 0
 	}
 	
-	function clock(t, process){
-	  var now = new Date().getTime(),
-	  		timer;
-	  
-	  if (debug) { console.log('now: ', now, 't: ', t); }
-	  
-	  if (t > now){ //(now - process.latency) ) {
-	  	timer = setTimeout(function(){
-	  		var now = new Date().getTime();
-	  		
-	  		//process.latency = now - t;
-	  		process.playProcessQueue();
-	  		
-	  		clearTimeout(timer);
-	  		timer = null;
-	  		
-	  		console.log('latency: ', now - t);
-	  		
-	  		process.makeProcessQueue(t, clock);
-	  	}, t - now ); //- process.latency)
-	  }
-	  else {
-	  	process.playProcessQueue();
-	  	console.log('latency: ', now - t);
-	  	process.makeProcessQueue(t, clock);
-	  }
+	var timer;
+	
+	function clock(process, t){
+		var now = new Date().getTime();
+		
+		if (debug) { console.log('now: ', now, 't: ', t); }
+		
+		if ( t > now ) { //(now - transport.latency) ) {
+			timer = setTimeout(function(){
+				var now = new Date().getTime(),
+						latency = now - t;
+				
+				transport.latency = latency;
+				
+				playProcessQueue(process);
+				clearTimeout(timer);
+				timer = null;
+				
+				console.log('latency: ', latency);
+				makeProcessQueue(process, t, clock);
+			}, t - now ); //- transport.latency);
+		}
+		else {
+			console.log('latency: ', now - t, ' - buffered');
+			playProcessQueue(process);
+			makeProcessQueue(process, t, clock);
+		}
 	}
 	
-	function kill( fn ) {
-		fn(this);
-	}
-	
-	function makeProcessQueue(t, fn) {
+	function makeProcessQueue(process, t, fn) {
 		var processQueue,
-				limit = t + 10000,
+				limit = t + 20000,
 				l;
 		
 		// Scan ahead looking for next eventList
 		while (t++ < limit) {
-			processQueue = this.getProcessList(t);
+			processQueue = process.getProcessList(t);
 			l = processQueue.length;
 			
 			if ( l ) {
-				fn(t, this);
-				break;
+				return fn(process, t);
 			}
 		}
+		
+		if (debug) { console.log('No events in the next 20 seconds. Stopping.'); }
 	}
 	
-	function playProcessQueue() {
-		var processQueue = this.processQueue,
+	function playProcessQueue(process) {
+		var processQueue = process.processQueue,
 				l = processQueue.length;
 		
 		// Trigger the processes
@@ -67,59 +74,82 @@
 		}
 	}
 	
-	function getProcessList(t) {
-		var sequence = this.sequence,
-				children = this.children,
-				t = t - this.starttime,
-				l,
-				events, k, event,
-				processList = [];
-		
-		//if (debug) { console.log('starttime: ' + this.starttime, 't: ' + t, 'events: ', sequence[t]); }
-		
-		events = sequence[t];
-		
-		if ( events ) {
-			k = events.length;
-			
-			while (k--) {
-				event = events[k];
-				process = new processMap[ event.type ]( event, t );
-				if (process instanceof SequenceProcess) {
-					children.push(process);
-				}
-				else {
-					processList.push(process);
-				}
-			}
-		}
-		
-		l = children.length;
-		
-		while (l--) {
-			processList = processList.concat( children[l].getProcessList(t) );
-		}
-		
-		this.processQueue = processList;
-		
-		return processList;
-	}
+	// Processes --------------------------------------
+	
+	var processes = {
+		'click': DOMEventProcess,
+		'audio': AudioEventProcess,
+		'sequence': SequenceProcess
+	};
+	
+	var selectors = {};
+	
+	var audiosrcs = {};
 	
 	function Process() {
 	
 	}
 	
-	Process.prototype = {
-		kill: kill
-	};
+	Process.prototype = {};
 	
 	function DOMEventProcess(e) {
+		
+		var obj;
+		
+		// Cache the jQuery object corresponding to this selector
+		// This could be dangerous if the DOM changes! We need a
+		// way to intelligently flush the cache!!
+		if ( e.selector ) {
+			if ( selectors[e.selector] ) {
+				obj = selectors[e.selector];
+			}
+			else {
+				obj = selectors[e.selector] = jQuery(e.target || e.selector);
+			}
+		}
+		else {
+			obj = jQuery(e.target);
+		}
+		
 		return function(){
-			jQuery(e.target || e.selector).trigger(e);
+			obj.trigger(e);
 		}
 	}
 	
 	DOMEventProcess.prototype = new Process();
+
+	function AudioEventProcess(e) {
+		var obj, canplay = false;
+		
+		if (debug) { console.log('[AudioEventProcess] '+e.src, 'Initialisation'); }
+		
+		// Cache the Audio obj for this sound src
+		if ( e.src ) {
+			if ( audiosrcs[e.src] ) {
+				obj = audiosrcs[e.src];
+			}
+			else {
+				obj = new Audio(e.src);
+				obj.oncanplay = function(){
+					canplay = true;
+					if (debug) { console.log('[AudioEventProcess] '+e.src, 'canplay'); }
+				};
+				if (debug) { console.log('[AudioEventProcess] '+e.src, 'Created new Audio object'); }
+			}
+		}
+		
+		return function(){
+			if (canplay) { obj.play(); }
+			else {
+				obj.oncanplay = function(){
+					if (debug) { console.log('[AudioEventProcess] '+e.src, 'canplay and play()'); }
+					obj.play();
+				}
+			}
+		}
+	}
+	
+	AudioEventProcess.prototype = new Process();
 	
 	function SequenceProcess(e, t) {
 		this.starttime = t;
@@ -131,21 +161,52 @@
 	SequenceProcess.prototype = new Process();
 	
 	jQuery.extend(SequenceProcess.prototype, {
-		makeProcessQueue: makeProcessQueue,
-		playProcessQueue: playProcessQueue,
-		getProcessList: getProcessList,
-		play: function(){
-			var now = new Date().getTime();
+		getProcessList: 	function getProcessList(t) {
+			var sequence = this.sequence,
+					children = this.children,
+					t = t - this.starttime,
+					l,
+					events, k, event,
+					processList = [];
 			
-			this.starttime = now;
+			events = sequence[t];
 			
-			console.log('now: ', now);
+			if ( events ) {
+				k = events.length;
+				
+				while (k--) {
+					event = events[k];
+					process = new processes[ event.type ]( event, t );
+					if (process instanceof SequenceProcess) {
+						children.push(process);
+					}
+					else {
+						processList.push(process);
+					}
+				}
+			}
 			
-			this.makeProcessQueue(now, clock);
-		},
-		
+			l = children.length;
+			
+			while (l--) {
+				processList = processList.concat( children[l].getProcessList(t) );
+			}
+			
+			this.processQueue = processList;
+			
+			return processList;
+		}
 	});
 	
-	window.Sequencer = SequenceProcess;
+	function Sequencer(sequence){
+		var process = new SequenceProcess(sequence);
+		jQuery.extend(process, transport);
+		return process;
+	}
+	
+	Sequencer.processes = processes;
+	
+	// Expose Sequencer to global scope
+	window.Sequencer = Sequencer;
 	
 })();
