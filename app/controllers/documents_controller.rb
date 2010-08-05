74f7c6c9a0aa1864352e09cf05c9f924b11eaee5
Jonathan Biolaz <jonathan.biolaz@webdoc.com>
2010-08-05 16:03:51 +0200

    added more from this author in document show+

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -121,6 +121,7 @@ class DocumentsController < ApplicationController
     end
     if @document.present?
       @related_documents = Document.all(:conditions => { :category_id => @document.category_id}, :limit => 12 )
+      @more_author_documents = Document.all(:conditions => { :creator_id => @document.creator_id}, :limit => 6 )
       respond_to do |format|
         format.html do
           @get_return_to = get_return_to 
