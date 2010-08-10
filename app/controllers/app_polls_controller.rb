81ef065dbb986f8ec90bd74d59268ac59a6fc3c3
Cédric Michelet <cedric.michelet@icare.info>
2010-08-10 13:11:37 +0200

    first implementation app_polls_controller

--- app/controllers/app_polls_controller.rb
+++ app/controllers/app_polls_controller.rb
@@ -1,2 +1,61 @@
 class AppPollsController < ApplicationController
+  
+  #GET /app_polls?item_id => return all votes : accessible only to editors
+  #GET /app_polls?item_id&only_current_user=true => return the vote of the current user
+  #GET /app_polls?item_id&summary=true => return sumarry of votes (total of votes by choice)
+  def index
+    if current_user
+      #MODE: vote for current user
+      if params[:only_current_user] == true || params[:only_current_user] == 'true'
+        app_polls = AppPoll.where(:item_id => params[:item_id], :user_id => current_user.uuid)
+        render :json => app_polls
+      #MODE: summary of all votes
+      elsif params[:summary] == true || params[:summary] == 'true'
+        app_polls = AppPoll.where(:item_id => params[:item_id])
+        hash = {}
+        total = 0
+        app_polls.each do |p|
+          choices = p.choices.split(',')
+          choices.each do |c|
+              total += 1
+              hash[c] = hash[c] ? hash[c] + 1 : 1
+          end
+        end
+        hash[:total] = total
+        render :json => hash
+      #MODE: all votes
+      else
+        #TODO - test if user is editor
+        app_polls = AppPoll.where(:item_id => params[:item_id])
+        render :json => app_polls
+      end
+    end
+  end
+  
+  #POST /app_polls?item_id&choices&other&geolocalisation
+  #Add or update a vote for current user
+  def create
+    if current_user
+      app_poll = AppPoll.where(:item_id => params[:item_id],:user_id => current_user.uuid).first || AppPoll.new
+      
+      #TODO - check user has right to vote on the poll, and that the item_id is a correct Poll item
+      app_poll.item_id = params[:item_id]
+      
+      app_poll.user_id = current_user.uuid
+      app_poll.choices = params[:choices]
+      app_poll.other = params[:other]
+      app_poll.geolocation = params[:geolocation]
+      app_poll.save
+      
+      render :text => ''
+    end
+  end
+  
+  #DELETE /app_polls?item_id
+  #Delete all votes - accessible only to editors
+  def destroy
+    AppPoll.destroy_all(:item_id => params[:item_id])
+    render :text => ''
+  end
+  
 end
