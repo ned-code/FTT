255d1c884d1e3c445833b9507e73bc8c7bb4b30a
Jonathan Biolaz <jonathan.biolaz@webdoc.com>
2010-08-11 14:54:20 +0200

    added migrations for new role

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,45 +2,34 @@ class Role < ActiveRecord::Base
   has_uuid
   set_primary_key :uuid
     
-  attr_accessible :name, :authorizable, :authorizable_id, :authorizable_type, :uuid
+  attr_accessible :name, :uuid, :user_id#, :document_id, :item_id, :user_list_id
   
   # ================
   # = Associations =
   # ================
   
-  has_many :roles_users
+  #has_many :roles_users
   
   # =================
   # = Class Methods =
   # =================
   
-  def self.all_by_user_document_ids_grouped_by_name(user)
-    roles = all(
-      :select     => "authorizable_id AS document_id, name",
-      :joins      => :roles_users,
-      :conditions => {
-        :roles       => { :authorizable_type => "Document" },
-        :roles_users => { :user_id => user.uuid }
-      }
-    ).group_by { |role| role.name }
-    roles.each { |k,v| v.map! { |role| role.document_id } }
-    roles
-  end
+  # Methods used with acl9
+  #
+  # def self.all_by_user_document_ids_grouped_by_name(user)
+  #   roles = all(
+  #     :select     => "authorizable_id AS document_id, name",
+  #     :joins      => :roles_users,
+  #     :conditions => {
+  #       :roles       => { :authorizable_type => "Document" },
+  #       :roles_users => { :user_id => user.uuid }
+  #     }
+  #   ).group_by { |role| role.name }
+  #   roles.each { |k,v| v.map! { |role| role.document_id } }
+  #   roles
+  # end
   
 end
 
 
-
-
-# == Schema Information
-#
-# Table name: roles
-#
-#  name              :string(40)
-#  authorizable_type :string(40)
-#  authorizable_id   :string(36)
-#  created_at        :datetime
-#  updated_at        :datetime
-#  uuid              :string(36)      default(""), not null, primary key
-#
 
