fec3ecea510095ea2c47051a75b8a9e6e5e5dc6b
Jonathan Biolaz <jonathan.biolaz@webdoc.com>
2010-08-12 14:30:46 +0200

    update app_polls_controller.rb

--- app/controllers/app_polls_controller.rb
+++ app/controllers/app_polls_controller.rb
@@ -4,41 +4,37 @@ class AppPollsController < ApplicationController
   #GET /app_polls?item_id&only_current_user=true => return the vote of the current user
   #GET /app_polls?item_id&summary=true => return sumarry of votes (total of votes by choice)
   def index
-    if current_user
-      #MODE: vote for current user
-      if params[:only_current_user] == true || params[:only_current_user] == 'true'
-        app_polls = AppPoll.where(:item_id => params[:item_id], :user_id => current_user.uuid)
-        render :json => app_polls
-      #MODE: summary of all votes
-      elsif params[:summary] == true || params[:summary] == 'true'
-        app_polls = AppPoll.where(:item_id => params[:item_id])
-        hash = {}
-        total = 0
-        app_polls.each do |p|
-          choices = p.choices.split(',')
-          choices.each do |c|
-              total += 1
-              hash[c] = hash[c] ? hash[c] + 1 : 1
-          end
+    #MODE: vote for current user
+    if params[:only_current_user] == true || params[:only_current_user] == 'true'
+      app_polls = AppPoll.where(:item_id => params[:item_id], :user_id => current_user.uuid)
+      render :json => app_polls
+    #MODE: summary of all votes
+    elsif params[:summary] == true || params[:summary] == 'true'
+      app_polls = AppPoll.where(:item_id => params[:item_id])
+      hash = {}
+      total = 0
+      app_polls.each do |p|
+        choices = p.choices.split(',')
+        choices.each do |c|
+            total += 1
+            hash[c] = hash[c] ? hash[c] + 1 : 1
         end
-        hash[:total] = total
-        render :json => hash
-      #MODE: all votes
-      else
-        #TODO - test if user is editor
-        app_polls = AppPoll.where(:item_id => params[:item_id])
-        render :json => app_polls
       end
+      hash[:total] = total
+      render :json => hash
+    #MODE: all votes
+    else
+      #TODO - test if user is editor
+      app_polls = AppPoll.where(:item_id => params[:item_id])
+      render :json => app_polls
     end
   end
   
   #POST /app_polls?item_id&choices&other&geolocalisation
   #Add or update a vote for current user
   def create
-    if current_user
-      app_poll = AppPoll.create_or_update(current_user,params)
-      render :text => ''
-    end
+    app_poll = AppPoll.create_or_update(current_user,params)
+    render :text => ''
   end
   
   #DELETE /app_polls?item_id
