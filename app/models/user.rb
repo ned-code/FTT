3d3891cf0d24c1445b01a76a10834b8a802e9aa2
Stephen <stephen.band@mnemis.com>
2010-08-13 17:01:12 +0200

    Adds user thumb view.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   avatars_path = "uploads/user/avatar/:id/:cw_style:basename.:extension"
   has_attached_file :avatar,
                     :styles => { :thumb=> "128x128#" },
-                    :default_url   => "/images/default_thumb_128.png",
+                    :default_url   => "/images/thumbs/default_thumb.png",
                     :storage => S3_CONFIG[:storage].to_sym,
                     :s3_credentials => S3_CONFIG,
                     :bucket => S3_CONFIG[:assets_bucket],
