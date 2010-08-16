0f2590f38b42928ed8ce76bd97ea850fa8b7a441
Dmitry Dudin <dudin@xbsoftware.com>
2010-08-16 18:10:52 +0200

    YouTube and custom flv drugging support for webVideoPlayer

--- app/javascripts/webdoc/controllers/board_controller.js
+++ app/javascripts/webdoc/controllers/board_controller.js
@@ -703,9 +703,6 @@ WebDoc.BoardController = jQuery.klass({
   insertVideo: function(videoProperties, position) {
     var videoWidget;
     switch (videoProperties.type) {
-      case 'youtube' :
-        videoWidget = WebDoc.WidgetManager.getInstance().getYoutubeWidget();
-        break;
       case 'video' :
         videoWidget = WebDoc.WidgetManager.getInstance().getVideoWidget();
         break;
--- app/javascripts/webdoc/core/widget_manager.js
+++ app/javascripts/webdoc/core/widget_manager.js
@@ -7,30 +7,11 @@ WebDoc.WidgetManager = $.klass(
   initialize: function(callBack)    
   {
     this._callBack = callBack;
-    this._initYoutube();
     this._initVideo();  
   },
 
   getVideoWidget: function() {
     return WebDoc.WidgetManager.videoWidget;   
-  },
-  
-  getYoutubeWidget: function() {
-    return WebDoc.WidgetManager.youtubeWidget;   
-  },
-
-  _initYoutube: function() {
-    if (WebDoc.WidgetManager.youtubeWidget === undefined) {
-      WebDoc.ServerManager.getRecords(WebDoc.Widget, 'youtube', function(data) {
-        if (data && data.length > 0) {
-          WebDoc.WidgetManager.youtubeWidget = data[0];
-        }
-        else {
-          WebDoc.WidgetManager.youtubeWidget = null;
-        }
-        this._readyForCallBack();
-      }.pBind(this));
-    }  
   },
   
    _initVideo: function() {
@@ -48,12 +29,7 @@ WebDoc.WidgetManager = $.klass(
   },
   
   _readyForCallBack: function() {
-    if (WebDoc.WidgetManager.youtubeWidget !== undefined && 
-        WebDoc.WidgetManager.vimeoWidget !== undefined && 
-        WebDoc.WidgetManager.dailymotionWidget !== undefined &&
-        WebDoc.WidgetManager.vidsMyspaceWidget !== undefined &&
-        WebDoc.WidgetManager.metacafeWidget !== undefined &&
-        WebDoc.WidgetManager.vidsGoogleWidget !== undefined) {
+    if (WebDoc.WidgetManager.videoWidget !== undefined) {
       this._callBack.call(this, WebDoc.WidgetManager);
     }
   }
