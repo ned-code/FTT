6b3d53f642e4327902917f1e4d6e084905fe1748
Stephen <stephen.band@mnemis.com>
2010-08-20 13:59:51 +0200

    Stuff

--- app/javascripts/webdoc/controllers/panel_controllers/inspector_controller.js
+++ app/javascripts/webdoc/controllers/panel_controllers/inspector_controller.js
@@ -37,9 +37,10 @@ WebDoc.InspectorController = $.klass(WebDoc.RightBarInspectorController, {
   _updateInspector: function(inspectorId) {
     var inspector;
     
-    ddd("[InspectorController] updatePalette", inspectorId );
-    if ( this._inspectorNodes[inspectorId] === undefined ) {
-      inspectorId = 'empty';
+    ddd("[InspectorController] _updateInspector", inspectorId );
+    
+    if ( typeof this._inspectorNodes[inspectorId] === 'undefined' ) {
+      WebDoc.application.panelsController.deselectInspector();
     }
     if (inspectorId !== this.currentInspectorId) {
       this.currentInspectorId = inspectorId;
@@ -66,12 +67,15 @@ WebDoc.InspectorController = $.klass(WebDoc.RightBarInspectorController, {
     ddd( "selected item ", selection );
     
     //we show the inspector only if there is one item in the selection
-    if ( selection.length === 1) {
-    	ddd('[] selectionChanged', selection[0].inspectorGroupName());
+    if ( selection.length === 0 ) {
+      this._updateInspector();
+    }
+    else if ( selection.length === 1 ) {
+    	ddd('[InspectorController] selectionChanged', selection[0].inspectorGroupName());
       this._updateInspector( selection[0].inspectorGroupName() );
     }
     else {
-      this._updateInspector( 'empty' );
+      this._updateInspector('empty');
     }
   }
 });
