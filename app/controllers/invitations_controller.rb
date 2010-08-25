e4718aae67f4488dd5b4d69592dc0f752fac68b1
Jonathan Biolaz <jonathan.biolaz@webdoc.com>
2010-08-25 13:42:42 +0200

    added index invitations for user and new form

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,14 +1,29 @@
 class InvitationsController < ApplicationController
   before_filter :authenticate_user!, :only => :create
   skip_before_filter :process_invitation
+  
   def index
     @invitations = current_user.invitations
   end
   
+  def new
+    @invitation = Invitation.new
+  end
   def create
     if params[:invitation].present?
       Invitation.generate(current_user, params[:invitation])
     end
-    render :json => {}
+    respond_to do |format|
+      format.json { render :json => {} }
+      format.html { redirect_to invitations_path }
+    end
+    
+  end
+
+  def destroy
+    @invitation = Invitation.where( :uuid => params[:id]).first
+    @invitation.destroy
+    flash[:notice] = 'Invitation removed'    
+    redirect_to invitations_path
   end
 end
