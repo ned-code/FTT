687f33d70126f93a0d7e0d20e9e9e00fc22007f0
Ilya Basiuk <ilyabasiuk@gmail.com>
2011-08-25 19:06:19 +0200

    writing user solution of critical data, add function getCoordByProp

--- joomla/components/com_manager/php/gedcom/classes/class.conflicts.manager.php
+++ joomla/components/com_manager/php/gedcom/classes/class.conflicts.manager.php
@@ -66,6 +66,103 @@ class ConflictsList {
         $conflict->Value = $row['value'];
         return $conflict;
     }
+     public function getPropCoord($prop,$owner_id=null){
+       $prop = explode(".",$prop);
+       $table=null;
+       $field=null;
+       $id=null;
+       switch (count($prop)){
+            case 1:{
+               if ($prop[0]=='Gender') {
+                    $table=' #__mb_individuals ';
+                    $field=' `sex` '; 
+               } else {
+                    $table=' #__mb_names ';
+                    switch ($prop[0]){
+                        case 'FirstName': $field=' `first_name` '; break;
+                        case 'LastName': $field=' `last_name` '; break;
+                        case 'Nick': $field=' `nick` '; break;
+                        case 'MiddleName': $field=' `miidle_name` '; break;
+                        case 'Prefix': $field=' `prefix` '; break;
+                        case 'Suffix': $field=' `suffix` '; break;
+                        case 'SurnamePrefix': $field=' `surn_prefix` '; break;                        
+                    }
+               }
+               $id = $owner_id;
+            break;}
+            case 2:{
+               if ($prop[1]=='DateType'){
+                    $table=' #__mb_dates ';
+                    $field=' `type` ';
+               } else {
+                    $table=' #__mb_events ';
+                    switch ($prop[1]){
+                        case 'Caus': $field=' `caus` '; break;
+                        case 'Type': $field=' `type` '; break;
+                        case 'ResAgency': $field=' `res_agency` '; break;                    
+                    }
+               }
+               if ($owner_id!=null){
+                    switch ($prop[0]){
+                        case 'Birth': $type='BIRT'; break;
+                        case 'Death': $type='DEAT'; break;
+                    }
+                    $this->core->sql('SELECT * FROM #__mb_events WHERE `individuals_id`=? and `type`=?',$owner_id,$type);
+                    $this->db->setQuery($sql);
+                    $rows = $this->db->loadAssocList();
+                    $id = $rows[0]['id'];    
+               } 
+            break;}
+            case 3:{
+               if ($prop[1]=='Place'){
+                    if ($prop[2]=='Name'){
+                        $table = ' #__mb_places';
+                        $field= ' `name` ';
+                    } else {
+                      $table=' #__mb_locations ';
+                      switch ($prop[2]){
+                          case 'Adr1': $field=' `adr1` '; break;
+                          case 'Adr2': $field=' `adr2` '; break;
+                          case 'City': $field=' `city` '; break;
+                          case 'Country': $field=' `country` '; break;
+                          case 'LocationName': $field=' `name` '; break;
+                          case 'Post': $field=' `post` '; break;
+                          case 'State': $field=' `state` '; break;                                                                                                                                                    
+                      }  
+                    }
+               } else {
+                    $table = ' #__mb_dates ';
+                    switch ($prop[1]){
+                        case 'From': $field=' `f_'; break;
+                        case 'To': $field=' `t_'; break;
+                    }
+                    switch ($prop[2]){
+                        case 'Day': $field.='day` '; break;
+                        case 'Month': $field.='month` '; break;
+                        case 'Year': $field.='year` '; break;
+                    }
+                   if ($owner_id!=null){
+                        switch ($prop[0]){
+                            case 'Birth': $type='BIRT'; break;
+                            case 'Death': $type='DEAT'; break;
+                        }
+                        $sql=$this->core->sql('SELECT * FROM #__mb_events WHERE `individuals_id`=? and `type`=?',$owner_id,$type);
+                        $this->db->setQuery($sql);
+                        $rows = $this->db->loadAssocList();
+                        $id = $rows[0]['id'];    
+                   } 
+               }
+            break;}  
+       }
+       switch ($table) {
+            case ' #__mb_dates ': $key_field=' `events_id` '; break;
+            case ' #__mb_locations ': $key_field=' `place_id` '; break;
+            case ' #__mb_names ': $key_field=' `gid` '; break;
+            case ' #__mb_events ': $key_field=' `id` '; break;
+            case ' #__mb_individuals ': $key_field=' `id` '; break;
+       }
+      return array("table"=>$table,"field"=>$field,"id"=>$id,"key_field"=>$key_field);    
+    }
 }
 
 
