75ffe69f704b1797b67049f91be3ab9c72dfae33
AlexanderPotashko <fantomhp@gmail.com>
2011-10-19 16:50:32 +0300

    update famous_family backend module.

--- joomla/components/com_manager/modules/famous_family_backend/js/famous.family.backend.js
+++ joomla/components/com_manager/modules/famous_family_backend/js/famous.family.backend.js
@@ -1,6 +1,13 @@
 function FamousFamilyBackend(obj){
+	var module = this;
 	obj = jQuery('#'+obj);
-	var html = function(){
+	
+	if(jQuery("#iframe-famous-family").length==0){
+		var iframe = '<iframe id="iframe-famous-family" name="#iframe-famous-family" style="display:none;position:absolute;left:-1000px;width:1px;height:1px">';
+		jQuery(obj).append(iframe);
+	}
+
+ 	var createBody = function(){
 		var sb = host.stringBuffer();
 		sb._('<table width="100%" cellspacing="0" cellpadding="0">');
 			sb._('<tr><td colspan="2"><div class="jmb-famous-family-backend-header"><span>Famous Family Trees - Administration</span></div></td></tr>');
@@ -8,59 +15,145 @@ function FamousFamilyBackend(obj){
 			sb._('<td valign="top" style="width:150px;">');
 					sb._('<div class="jmb-famous-family-backend-tree">');
 						sb._('<div class="jmb-famous-family-backend-tree-header"><span>Famous Family Trees</span></div>');
-						sb._('<div class="jmb-famous-family-backend-tree-item"><span>Elvis Presley</span></div>');
-						sb._('<div class="jmb-famous-family-backend-tree-item"><span>Elvis Presley</span></div>');
-						sb._('<div class="jmb-famous-family-backend-tree-item"><span>Elvis Presley</span></div>');
-						sb._('<div class="jmb-famous-family-backend-tree-item"><span>Elvis Presley</span></div>');
-						sb._('<div class="jmb-famous-family-backend-tree-item"><span>Elvis Presley</span></div>');
 					sb._('</div>');
 					sb._('<div class="jmb-famous-family-backend-tree-add"><span>Add New Tree</span></div>')
 				sb._('</td>');
 				sb._('<td>');
 					sb._('<div class="jmb-famous-family-backend-content">');
-						sb._('<div class="jmb-famous-family-backend-content-item">')
-							sb._('<div><span>Tree Name:</span></div>');
-							sb._('<div><input></div>');
-						sb._('</div>');	
-						sb._('<div class="jmb-famous-family-backend-content-item">')
-							sb._('<div><span>Key Member:</span></div>');
-							sb._('<div><input></div>');
-						sb._('</div>');	
-						sb._('<div class="jmb-famous-family-backend-content-item">')
-							sb._('<div><span>Description:</span></div>');
-							sb._('<div><textarea></textarea></div>');
-						sb._('</div>');	
-						sb._('<div class="jmb-famous-family-backend-content-item">')
-							sb._('<div><span>Who can see this tree:</span></div>');
-							sb._('<div><select><option>Everyone</option><option>Tree Kepers</option><option>Nobody</option></select></div>');
-						sb._('</div>');	
-						sb._('<div class="jmb-famous-family-backend-content-keepers">')
-							sb._('<div class="header"><span>Tree Keepers:</span></div>');
-							sb._('<div class="body">');
-								sb._('<table border="1" bordercolor="#999" width="100%" cellspacing="0" cellpadding="0">');
-									sb._('<tr class="head">');
-										sb._('<td><div><span>Name</span></div></td>');
-										sb._('<td><div><span>Last activity</span></div></td>');
-										sb._('<td><div><span>&nbsp;</span></div></td>');
-									sb._('<tr>');
-									sb._('<tr>');
-										sb._('<td><div><span>Greeg William</span></div></td>');
-										sb._('<td><div><span>5 days ago</span></div></td>');
-										sb._('<td><div><span>x</span></div></td>');
-									sb._('<tr>');
-								sb._('</table>');
-								sb._('<div class="jmb-famous-family-backend-content-keepers-add"><span>Add Tree Keeper</span></div>');
-							sb._('</div>');
-						sb._('</div>');	
+						
 					sb._('</div>');
 				sb._('</td>');
 			sb._('</tr>');
 		sb._('</table>');
 		return jQuery(sb.result());
 	}
-
-	var htmlObject = html();
-	jQuery(obj).append(htmlObject);
+	var getName = function(object){ return [object.first_name, object.middle_name, object.last_name].join(' ') }
+	var getTime = function(time){ 
+		var t = time.split(/[- :]/); 
+		return (t[0]!='0000'&&t[1]!='00'&&t[2]!='00')?new Date(t[0], t[1]-1, t[2], t[3], t[4], t[5]): false; 
+	}
+	var getLastLogin = function(object, time){
+		var now = getTime(time);
+		var t   = getTime(object.last_login);
+		if(!now||!t) return 'unknown';
+		var dif = Math.round((now.getTime() - t.getTime())/1000);
+		var d = Math.floor(dif/86400);
+		var h = Math.floor(dif/3600%24);
+		return (d!=0)?d+' days '+h+' hours ago':h+' hors ago';
+	}
+	var setSelectOption = function(content, permission){
+		jQuery(content).find('select[name="permission"] option[value="'+permission+'"]').attr('selected', 'selected');
+	}
+	var createNewContent = function(html){
+		var box = jQuery(html).find('.jmb-famous-family-backend-content');
+		var sb = host.stringBuffer();
+		jQuery(box).html('');
+		//create html
+		sb._('<form id="jmb_create_famous_family" method="post" target="iframe-famous-family">');
+			sb._('<div class="jmb-famous-family-backend-buttons"><input type="submit" value="Save"></div>');
+			sb._('<div class="jmb-famous-family-backend-content-item">')
+				sb._('<div><span>Tree Name:</span></div>');
+				sb._('<div><input name="name" value=""></div>');
+			sb._('</div>');	
+			sb._('<div class="jmb-famous-family-backend-content-item">')
+				sb._('<div><span>Key Member:</span></div>');
+				sb._('<div><input name="individuals_id" value=""></div>');
+			sb._('</div>');	
+			sb._('<div class="jmb-famous-family-backend-content-item">')
+				sb._('<div><span>Description:</span></div>');
+				sb._('<div><textarea name="description"></textarea></div>');
+			sb._('</div>');	
+			sb._('<div class="jmb-famous-family-backend-content-item">')
+				sb._('<div><span>Who can see this tree:</span></div>');
+				sb._('<div><select name="permission"><option value="2">Everyone</option><option value="1">Tree Kepers</option><option value="0">Nobody</option></select></div>');
+			sb._('</div>');	
+		sb._('</form>');
+		var content = jQuery(sb.result());
+		module.ajaxForm(content, 'createNewFamousFamily', null, function(res){
+			alert(res.message);
+		});
+		jQuery(box).append(content);
+	}
+	
+	var createContent = function(object, html, time){
+		var box = jQuery(html).find('.jmb-famous-family-backend-content');
+		var sb = host.stringBuffer();
+		jQuery(box).html('');
+		//create html
+		sb._('<form id="jmb_create_famous_family" method="post" target="iframe-famous-family">');
+			sb._('<div class="jmb-famous-family-backend-buttons"><input type="submit" value="Save"><input type="button" value="Delete"></div>');
+			sb._('<div class="jmb-famous-family-backend-content-item">')
+				sb._('<div><span>Tree Name:</span></div>');
+				sb._('<div><input name="name" value="')._(object.name)._('"></div>');
+			sb._('</div>');	
+			sb._('<div class="jmb-famous-family-backend-content-item">')
+				sb._('<div><span>Key Member:</span></div>');
+				sb._('<div><input name="individuals_id" value="')._(object.individuals_id)._('"></div>');
+			sb._('</div>');	
+			sb._('<div class="jmb-famous-family-backend-content-item">')
+				sb._('<div><span>Description:</span></div>');
+				sb._('<div><textarea name="description">')._(object.description)._('</textarea></div>');
+			sb._('</div>');	
+			sb._('<div class="jmb-famous-family-backend-content-item">')
+				sb._('<div><span>Who can see this tree:</span></div>');
+				sb._('<div><select name="permission"><option value="2">Everyone</option><option value="1">Tree Kepers</option><option value="0">Nobody</option></select></div>');
+			sb._('</div>');	
+		sb._('</form>');
+		sb._('<div class="jmb-famous-family-backend-content-keepers">')
+			sb._('<div class="header"><span>Tree Keepers:</span></div>');
+			sb._('<div class="body">');
+				sb._('<table border="1" bordercolor="#999" width="100%" cellspacing="0" cellpadding="0">');
+					sb._('<tr class="head">');
+						sb._('<td><div><span>Name</span></div></td>');
+						sb._('<td><div><span>Last activity</span></div></td>');
+						sb._('<td><div><span>&nbsp;</span></div></td>');
+					sb._('</tr>');
+					if(object.keepers){
+						for(var key in object.keepers){
+							sb._('<tr>');
+								sb._('<td><div><span>')._(getName(object.keepers[key]))._('</span></div></td>');
+								sb._('<td><div><span>')._(getLastLogin(object.keepers[key], time))._('</span></div></td>');
+								sb._('<td style="width:22px;"><div id="')._(key)._('" class="delete"><span>&nbsp;</span></div></td>');
+							sb._('</tr>');
+						}
+					}
+				sb._('</table>');
+				sb._('<div class="jmb-famous-family-backend-content-keepers-add"><span>Add Tree Keeper</span></div>');
+			sb._('</div>');
+		sb._('</div>');	
+		var content = jQuery(sb.result())
+		setSelectOption(content, object.permission);
+		module.ajaxForm(content, 'save', object.id, function(res){
+			alert(res.message);
+		});
+		jQuery(box).append(content);
+	}
+	var createFamousFamiliesTree = function(json, html){
+		var box = jQuery(obj).find('.jmb-famous-family-backend-tree');
+		jQuery(json.families).each(function(i,e){
+			var div = jQuery('<div id="'+e.id+'" class="jmb-famous-family-backend-tree-item"><span>'+e.name+'</span></div>');
+			jQuery(box).append(div);
+		});	
+		jQuery(box).find('.jmb-famous-family-backend-tree-item span').click(function(){
+			if(jQuery(this).hasClass('active')) return false;
+			var id = jQuery(this).parent().attr('id');
+			jQuery(box).find('.jmb-famous-family-backend-tree-item span').removeClass('active');
+			jQuery(this).addClass('active');
+			createContent(json.sort_families[id], html, json.time);
+		});
+		jQuery(box).parent().find('.jmb-famous-family-backend-tree-add span').click(function(){
+			createNewContent(html);
+		});
+	}
+	
+	var html_object = createBody();	
+	module.ajax('getFamousFamiliesTree',null, function(res){
+		var json = jQuery.parseJSON(res.responseText);
+		createFamousFamiliesTree(json, html_object);
+	});
+	
+	
+	jQuery(obj).append(html_object);
 	
 }
 
@@ -69,6 +162,18 @@ FamousFamilyBackend.prototype = {
 		host.callMethod("famous_family_backend", "JMBFamousFamilyBackend", func, params, function(res){
 				callback(res);
 		})
+	},
+	ajaxForm:function(obj, method, args, success){
+		var sb = host.stringBuffer();
+		var url = sb._('index.php?option=com_manager&task=callMethod&module=famous_family_backend&class=JMBFamousFamilyBackend&method=')._(method)._('&args=')._(args).result();
+		jQuery(obj).ajaxForm({
+			url:url,
+			dataType:"json",
+			target:"#iframe-famous-family",
+			success:function(data){
+				success(data);
+			}
+		});
 	}
 }
 
--- joomla/components/com_manager/modules/famous_family_backend/php/famous_family_backend.php
+++ joomla/components/com_manager/modules/famous_family_backend/php/famous_family_backend.php
@@ -7,49 +7,87 @@ class JMBFamousFamilyBackend {
 
 	}
 	
-	protected function _getUserTreePermission($args){
+	protected function getTreeKeepers(){
 		$db =& JFactory::getDBO();
-		$sqlString = "SELECT type FROM #__mb_tree_links WHERE individuals_id = ? AND tree_id = ? LIMIT 1";
-		$sql = $this->host->gedcom->sql($sqlString, $args->Id, $args->TreeId);
-		$db->setQuery($sql);
-		$rows = $db->loadAssocList();
-		return $rows[0]['type'];
-	}
-	
-	protected function _getFamilies(){
-		$db =& JFactory::getDBO();
-		$sqlString = "SELECT id, name, tree_id, individuals_id, permission FROM #__mb_famous_family";
+		$sqlString = "SELECT keepers.id, keepers.famous_family, keepers.individuals_id, name.first_name, name.middle_name, name.last_name, ind.last_login FROM #__mb_tree_keepers as keepers
+				LEFT JOIN #__mb_individuals as ind ON ind.id = keepers.individuals_id
+				LEFT JOIN #__mb_names as name ON name.gid = keepers.individuals_id";
 		$sql = $sqlString;
 		$db->setQuery($sql);
 		$rows = $db->loadAssocList();
-		return $rows;
+		if($rows == null) return array();
+		return $this->sortKeepers($rows);
 	}
 	
-	public function getFamilies(){
-		 $families = $this->_getFamilies();
-		 $result = array();
-		 foreach($families as $family){
-		 	 $ind = $this->host->gedcom->individuals->get($family['individuals_id']);
-		 	 $count = $this->host->gedcom->individuals->getIndivCount($family['tree_id']);
-		 	 $count = sizeof($count);
-		 	 $death = $this->host->gedcom->individuals->getDeathIndivCount($family['tree_id']);
-		 	 $living = $count - sizeof($death);
-		 	 $avatar = $this->host->gedcom->media->getAvatarImage($family['individuals_id']);
-		 	 $result[] = array('id'=>$family['id'],'name'=>$family['name'],'tree_id'=>$family['tree_id'],'individ'=>$ind,'descendants'=>$count,'living'=>$living,'avatar'=>$avatar);
-		 }
-		 $path = JURI::root(true);
-		 return json_encode(array('families'=>$result,'path'=>$path));
+	protected function sortKeepers($rows){
+		$result = array();
+		foreach($rows as $row){
+			$result[$row['famous_family']][$row['individuals_id']] = $row;
+		}
+		return $result;
 	}
 	
-	public function setFamilies($args){
-		$args = json_decode($args);
-		$permission = $this->_getUserTreePermission($args);
-		$_SESSION['jmb']['fid'] = '0';
-        	$_SESSION['jmb']['gid'] = $args->Id;
-		$_SESSION['jmb']['tid'] = $args->TreeId;
-		$_SESSION['jmb']['permission'] = $permission;
-		$_SESSION['jmb']['alias'] = 'myfamily';
-		$_SESSION['jmb']['login_type'] = 'famous_family';
+	public function getFamousFamiliesTree(){
+		$db =& JFactory::getDBO();
+		$sqlString = "SELECT id, name, tree_id, individuals_id, description, permission FROM #__mb_famous_family";
+		$sql = $sqlString;
+		$db->setQuery($sql);
+		$rows = $db->loadAssocList();
+		if($rows==null) return json_encode(array('error'=>'Famous families not found...'));
+		$keepers = $this->getTreeKeepers();
+		$result = array();
+		foreach($rows as $row){	
+			$result[$row['id']] = $row;
+			$result[$row['id']]['keepers'] = (isset($keepers[$row['id']]))?$keepers[$row['id']]:false;
+		}
+		$time = date('Y-m-d H:i:s');
+		return json_encode(array('families'=>$rows,'sort_families'=>$result,'time'=>$time));
 	}
+	
+	public function createNewFamousFamily(){
+		$db =& JFactory::getDBO();
+		$name = $_POST['name'];
+		$individuals_id = $_POST['individuals_id'];
+		$description = $_POST['description'];
+		$permission = $_POST['permission'];
+		
+		//get individuals 
+		$sqlString = "SELECT id FROM #__mb_individuals WHERE id = ? LIMIT 1";
+		$sql = $this->host->gedcom->sql($sqlString, $individuals_id);
+		$db->setQuery($sql);
+		$rows = $db->loadAssocList();
+		if($rows!=null&&strlen($name)!=0){
+			$sql = $this->host->gedcom->sql("INSERT INTO #__mb_tree (`id`,name) VALUES (NULL, ?)", $name);
+			$db->setQuery($sql);
+			$db->query();
+			$tree_id = $db->insertid();
+			
+			$sql = $this->host->gedcom->sql("INSERT INTO #__mb_tree_links (`individuals_id`, `tree_id`, `type`) VALUES (?,?, 'OWNER')", $individuals_id, $tree_id);
+			$db->setQuery($sql);
+			$db->query();
+			
+			$sql = $this->host->gedcom->sql("INSERT INTO #__mb_famous_family (`id`, `name`, `tree_id`, `individuals_id`, `description`, `permission`) VALUES (NULL, ?, ?, ?, ?, ?)", $name, $tree_id, $individuals_id, $description , $permission);
+			$db->setQuery($sql);
+			$db->query();
+			return json_encode(array('message'=>'Tree successfully saved!'));
+		}
+		return json_encode(array('message'=>'Tree save error.'));
+	}
+
+	public function save($id){
+		$db =& JFactory::getDBO();
+		$name = $_POST['name'];
+		$individuals_id = $_POST['individuals_id'];
+		$description = $_POST['description'];
+		$permission = $_POST['permission'];
+		
+		$sqlString = "UPDATE #__mb_famous_family SET `name` = ?, `individuals_id` = ?, `description` = ?, `permission` = ? WHERE  `id` =?";
+		$sql = $this->host->gedcom->sql($sqlString, $name, $individuals_id, $description, $permission, $id);
+		$db->setQuery($sql);
+		$db->query();
+		
+		return json_encode(array('message'=>'Tree data saved.'));
+	}
+	
 }
 ?>
