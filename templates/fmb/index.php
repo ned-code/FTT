cb92a06bf19d1c9100770e2c8c662f36e17cc1e4
AlexanderPotashko <fantomhp@gmail.com>
2012-07-25 17:05:29 +0200

    update feedback page.

--- templates/fmb/index.php
+++ templates/fmb/index.php
@@ -141,6 +141,26 @@ if (stripos($user_agent, 'MSIE 6.0') !== false
                         jQuery('div.footer').hide();
                     } else {
                         if('undefined' === typeof(storage)){
+                            var alias = '<?php echo $alias; ?>';
+                            if('feedback' === alias){
+                                jQuery('form#adminForm').ready(function(){
+                                    jQuery('form#adminForm').find('h2').remove();
+                                    var select = jQuery('form#adminForm').find('.forum-select');
+                                    jQuery(select).css('text-align', 'center');
+                                    jQuery(select).css('margin', '10px');
+                                    jQuery(select).find('div').css('float', 'none');
+                                    jQuery.ajax({
+                                        url:"index.php?option=com_manager&task=getLanguage&module_name=feedback",
+                                        type:"GET",
+                                        dataType: "html",
+                                        complete : function (req, err) {
+                                            var json = jQuery.parseJSON(req.responseText);
+                                            var string  = json.FTT_MOD_FEEDBACK_WELCOM.replace('%%', json.FTT_MOD_FEEDBACK_PUBLIC_BETA);
+                                            jQuery(select).before('<div style="font-size: 16px;font-weight: bold;margin: 30px;">'+string+'</div>');
+                                        }
+                                    });
+                                });
+                            }
                             jQuery('div.footer').show();
                         } else {
                             storage.core.modulesPullObject.bind(function(object){
