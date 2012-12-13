ef2e9616eb0b25535b2f07bf5f5ef33488fea3f2
AlexanderPotashko <fantomhp@gmail.com>
2012-12-13 12:55:08 +0200

    remove getLoginStatus. added check for fb.init in myfamily module.

--- components/com_manager/modules/myfamily/js/jmb.myfamily.js
+++ components/com_manager/modules/myfamily/js/jmb.myfamily.js
@@ -24,20 +24,26 @@ function JMBMyfamilyObject(parent){
             return '';
         },
         getFamilyList:function(callback){
-            FB.getLoginStatus(function(status){
-                if(status.status == "connected"){
-                    FB.api({
-                            method: 'fql.query',
-                            query: 'SELECT name, birthday, uid, relationship FROM family WHERE profile_id ='+storage.usertree.facebook_id
-                        },function(response) {
-                            callback(response);
-                        }
-                    );
-                } else {
-                    callback(false);
+            FB.api({
+                    method: 'fql.query',
+                    query: 'SELECT name, birthday, uid, relationship FROM family WHERE profile_id ='+storage.usertree.facebook_id
+                },function(response) {
+                    if(response.error_code){
+                        $fn.getFamilyList(callback);
+                    } else {
+                        callback(response);
+                    }
                 }
-
-            });
+            );
+        },
+        init:function(callback){
+            if("undefined" !== typeof(window.FB) ){
+                callback();
+            } else {
+                setTimeout(function(){
+                    $fn.init(callback);
+                }, 1000);
+            }
         },
         exit:function(){ storage.core.modulesPullObject.unset($moduleName); },
         reload:function(){ console.log("reload") },
@@ -71,39 +77,41 @@ function JMBMyfamilyObject(parent){
     $cont = $fn.create();
     jQuery(parent).append($cont);
 
-    $fn.getFamilyList(function(response){
-        if(!response) return false;
-        var table = jQuery('<table></table>');
-        var relations = {};
-        for(var key in response){
-            if(!response.hasOwnProperty(key)) continue;
-            var object = response[key];
-            relations[object.uid] = object.relationship;
-            var tr = jQuery('<tr id="'+object.uid+'"><div class="ftt-myfamily-list-item-load">&nbsp;</div></tr>')
-            jQuery(table).append(tr);
-            FB.api("/"+object.uid+"/feed", function(feed){
-                var data = feed.data[0],
-                    sb = storage.stringBuffer(),
-                    facebook_id = data.from.id,
-                    object,
-                    parse,
-                    relation;
-                if(object = $fn.userInSystem(facebook_id)){
-                    parse = storage.usertree.parse(object);
-                    relation = parse.relation;
-                } else {
-                    relation = relations[facebook_id];
-                }
-                sb._('<td><div class="ftt-myfamily-list-item-relation">')._(relation)._('</div></td>');
-                sb._('<td><div class="ftt-myfamily-list-item-avatar">')._(storage.usertree.avatar.facebook(facebook_id, 50, 50))._('</div></td>');
-                sb._('<td><div class="ftt-myfamily-list-item-text">')._(data.story)._('</div></td>');
-                jQuery(tr).find('div.ftt-myfamily-list-item-load').remove();
-                jQuery(tr).append(sb.result());
-            });
-        }
-        jQuery($cont).find(".ftt-myfamily-content").append(table);
+    $fn.init(function(){
+        $fn.getFamilyList(function(response){
+            if(!response) return false;
+            var table = jQuery('<table></table>');
+            var relations = {};
+            for(var key in response){
+                if(!response.hasOwnProperty(key)) continue;
+                var object = response[key];
+                relations[object.uid] = object.relationship;
+                var tr = jQuery('<tr id="'+object.uid+'"><div class="ftt-myfamily-list-item-load">&nbsp;</div></tr>')
+                jQuery(table).append(tr);
+                FB.api("/"+object.uid+"/feed", function(feed){
+                    var data = feed.data[0],
+                        sb = storage.stringBuffer(),
+                        facebook_id = data.from.id,
+                        object,
+                        parse,
+                        relation;
+                    if(object = $fn.userInSystem(facebook_id)){
+                        parse = storage.usertree.parse(object);
+                        relation = parse.relation;
+                    } else {
+                        relation = relations[facebook_id];
+                    }
+                    sb._('<td><div class="ftt-myfamily-list-item-relation">')._(relation)._('</div></td>');
+                    sb._('<td><div class="ftt-myfamily-list-item-avatar">')._(storage.usertree.avatar.facebook(facebook_id, 50, 50))._('</div></td>');
+                    sb._('<td><div class="ftt-myfamily-list-item-text">')._(data.story)._('</div></td>');
+                    jQuery(tr).find('div.ftt-myfamily-list-item-load').remove();
+                    jQuery(tr).append(sb.result());
+                });
+            }
+            jQuery($cont).find(".ftt-myfamily-content").append(table);
+        });
+        $fn.exit();
     });
-    $fn.exit();
 }
 
 JMBMyfamilyObject.prototype = {
