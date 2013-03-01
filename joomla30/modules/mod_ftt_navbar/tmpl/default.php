0e40b14a8e3cddd9bbf83d39d7eeaf6c159cf953
Alexander Potashko <fantomhp@gmail.com>
2013-03-01 17:59:59 +0300

    added profile menu to navbar

--- joomla30/modules/mod_ftt_navbar/tmpl/default.php
+++ joomla30/modules/mod_ftt_navbar/tmpl/default.php
@@ -1,5 +1,11 @@
 <?php
 defined('_JEXEC') or die;
+$user = FamilyTreeTopUserHelper::getInstance()->get();
+$facebook = FacebookHelper::getInstance()->facebook;
+$facebook_id = $facebook->getUseR();
+if(!empty($user->users) || !empty($user->users->gedcom_id)){
+    $ind = GedcomHelper::getInstance()->individuals->get($user->users->gedcom_id);
+}
 ?>
 <div class="navbar navbar-inverse">
     <div class="navbar-inner">
@@ -17,6 +23,22 @@ defined('_JEXEC') or die;
                     <li <?=($view=="index")?'class="active"':''; ?> ><a href="<?=JRoute::_("index.php?option=com_familytreetop&view=index", false); ?>">Home</a></li>
                 </ul>
             </div>
+            <?php if($facebook_id != 0): ?>
+                <ul class="nav pull-right">
+                    <li><img class="img-rounded" src="https://graph.facebook.com/<?=$facebook_id;?>/picture?width=40&height=40"/></li>
+                    <li class="divider-vertical"></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?=$ind->name();?> <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Profile</a></li>
+                            <li><a href="#">Languages</a></li>
+                            <li><a href="#">Redirect to Facebook</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Log Out</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            <?php endif; ?>
         </div>
     </div>
 </div>
