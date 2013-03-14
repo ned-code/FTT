ffd2938406027569be23c5dc95af9fbe9501473c
AlexanderPotashko <fantomhp@gmail.com>
2013-03-14 17:31:00 +0200

    update latest updates

--- joomla30/modules/mod_ftt_latest_updates/tmpl/default.php
+++ joomla30/modules/mod_ftt_latest_updates/tmpl/default.php
@@ -1,13 +1,19 @@
 <?php
 defined('_JEXEC') or die;
 
+$gedcom = GedcomHelper::getInstance();
+
+$profile = FamilyTreeTopIndividuals::find(array('order' => 'change_time desc'));
+$ind = $gedcom->individuals->get($profile->gedcom_id);
 ?>
 <div id="latestUpdates" class="row">
     <div class="span6">
         <div class="well">
             <fieldset>
                 <legend>Latest Updates</legend>
-               
+                <ul class="unstyled">
+                    <li><span>Profile Changes:</span> <span><?=$ind->name();?></span></li>
+                </ul>
             </fieldset>
         </div>
     </div>
