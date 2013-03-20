b9ae5cc868bf9661e782f76fe17fe51d7d9caaa7
AlexanderPotashko <fantomhp@gmail.com>
2013-03-20 16:06:14 +0200

    added name in nav bar by famous family module.

--- joomla30/modules/mod_ftt_header/tmpl/default.php
+++ joomla30/modules/mod_ftt_header/tmpl/default.php
@@ -1,11 +1,16 @@
 <?php
 defined('_JEXEC') or die;
+$gedcom = GedcomHelper::getInstance();
 $user = FamilyTreeTopUserHelper::getInstance()->get();
+$ind = $gedcom->individuals->get($user->gedcom_id);
 ?>
 <div class="navbar">
     <div class="navbar-inner">
         <?php if($user->famous): ?>
-            <a class="btn pull-right" href="<?=JRoute::_("index.php?option=com_familytreetop&task=famous.ext", false);?>" >Exit<a>
+            <div class="pull-right">
+                <span><?=$ind->name();?></span>
+                <a class="btn" href="<?=JRoute::_("index.php?option=com_familytreetop&task=famous.ext", false);?>" >Exit</a>
+            </div>
         <?php endif;?>
     </div>
 </div>
