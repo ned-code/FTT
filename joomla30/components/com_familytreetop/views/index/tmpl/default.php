d493a53457b95696bc3934ff95cfa6444de23123
AlexanderPotashko <fantomhp@gmail.com>
2013-04-15 15:19:12 +0200

    update home view.

--- joomla30/components/com_familytreetop/views/index/tmpl/default.php
+++ joomla30/components/com_familytreetop/views/index/tmpl/default.php
@@ -5,6 +5,9 @@ defined('_JEXEC') or die;
     $FamilyTreeTop.dataString = '<?=json_encode(GedcomHelper::getInstance()->getData());?>';
 </script>
 <div class="row-fluid">
+    <div class="span6 hidden-phone">
+        <img src="<?=$this->baseurl;?>/components/com_familytreetop/images/index.logo.jpg">
+    </div>
     <div class="span6">
         <div class="row-fluid">
             <div class="span12 text-center">
@@ -13,27 +16,19 @@ defined('_JEXEC') or die;
         </div>
         <div class="row-fluid">
             <div class="span12 text-center">
-                <hr>
-            </div>
-        </div>
-        <div class="row-fluid">
-            <div class="span12 text-center">
                 <a href="<?=JRoute::_("index.php?option=com_familytreetop&view=myfamily", false); ?>" class="btn btn-large btn-primary">My Family Tree</a>
                 <a href="<?=JRoute::_("index.php?option=com_familytreetop&view=famous", false); ?>" class="btn btn-large btn-info">Famous Family</a>
             </div>
         </div>
-        <div class="row-fluid">
-            <div class="span12 text-center">
-                <hr>
-            </div>
-        </div>
-        <div class="row-fluid">
-            <div class="span12 text-center">
-                <img src="<?=$this->baseurl;?>/components/com_familytreetop/images/index.ipad.png">
-            </div>
-        </div>
     </div>
-    <div class="span6">
+    <div class="span6 visible-phone">
         <img src="<?=$this->baseurl;?>/components/com_familytreetop/images/index.logo.jpg">
     </div>
 </div>
+<script>
+    $FamilyTreeTop.bind(function($){
+        'use strict';
+
+        
+    });
+</script>
