dff19cb6fe3482f5ad09ab5785288ac3519c8ece
fantome <fantomhp@gmail.com>
2013-04-17 09:25:36 +0300

    update myfamily module. added collapsing.

--- joomla30/modules/mod_ftt_myfamily/tmpl/default.php
+++ joomla30/modules/mod_ftt_myfamily/tmpl/default.php
@@ -39,58 +39,78 @@ foreach($data as $object){
             <fieldset>
                 <legend class="text-center">My Family on Facebook</legend>
             </fieldset>
-            <?php foreach($result_array as $object): ?>
-                <div class="row-fluid">
-                    <h4><?=$object['from']['name'];?></h4>
-                    <div class="row-fluid">
-                        <ul class="inline">
-                            <li class="span2"><img class="img-rounded" src="https://graph.facebook.com/<?=$object['from']['id'];?>/picture"/></li>
-                            <li class="span10">
-                                <p>
-                                    <?php
-                                        $message = isset($object['message'])?$object['message']:false;
-                                        $description = isset($object['description'])?$object['description']:false;
-                                        $story = isset($object['story'])?$object['story']:false;
-                                        $name = isset($object['name'])?$object['name']:false;
-                                        $ret = "";
+            <div id="accordion2" class="accordion">
+                <?php foreach($result_array as $object): ?>
+                    <div class="accordion-group">
+                        <div class="accordion-heading">
+                            <div class="row-fluid">
+                                <div class="span12">
+                                    <h4>
+                                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapse<?=$object['from']['id'];?>">
+                                            <?=$object['from']['name'];?>
+                                        </a>
+                                    </h4>   
+                                </div>
+                            </div>
+                            <div class="row-fluid">
+                                <div class="span12">
+                                    <ul class="inline">
+                                        <li class="span2"><img class="img-rounded" src="https://graph.facebook.com/<?=$object['from']['id'];?>/picture"/></li>
+                                        <li class="span10">
+                                            <p>
+                                                <?php
+                                                $message = isset($object['message'])?$object['message']:false;
+                                                $description = isset($object['description'])?$object['description']:false;
+                                                $story = isset($object['story'])?$object['story']:false;
+                                                $name = isset($object['name'])?$object['name']:false;
+                                                $ret = "";
 
-                                        if($message){
-                                            $ret = $message;
-                                        } elseif($story){
-                                            $ret = $story;
-                                        } elseif($description){
-                                            $ret = $description;
-                                        } elseif($name){
-                                            $ret = $name;
-                                        } else {
-                                            if(isset($object['type']) && $object['type'] == 'link'){
-                                               $ret = 'Likes on '. $object['application']['name'];
-                                            } else {
-                                               $ret = "";
-                                            }
-                                        }
-                                        if(strlen($ret) > 500){
-                                            $ret = substr($ret, 0 , 500) . "...";
-                                        }
-                                        echo $ret;
-                                    ?>
-                                </p>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="row-fluid">
-                        <div class="pull-right">
-                            <small>
-                                <?php
-                                    $date = $object['updated_time'];
-                                    echo date('j F \a\t H:i', strtotime($date));
-                                ?>
-                            </small>
+                                                if($message){
+                                                    $ret = $message;
+                                                } elseif($story){
+                                                    $ret = $story;
+                                                } elseif($description){
+                                                    $ret = $description;
+                                                } elseif($name){
+                                                    $ret = $name;
+                                                } else {
+                                                    if(isset($object['type']) && $object['type'] == 'link'){
+                                                        $ret = 'Likes on '. $object['application']['name'];
+                                                    } else {
+                                                        $ret = "";
+                                                    }
+                                                }
+                                                if(strlen($ret) > 500){
+                                                    $ret = substr($ret, 0 , 500) . "...";
+                                                }
+                                                echo $ret;
+                                                ?>
+                                            </p>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="row-fluid">
+                                <div class="span12">
+                                    <div class="pull-right">
+                                        <small>
+                                            <?php
+                                            $date = $object['updated_time'];
+                                            echo date('j F \a\t H:i', strtotime($date));
+                                            ?>
+                                        </small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="collapse<?=$object['from']['id'];?>" class="accordion-body collapse">
+                            <div class="accordion-inner">
+                                COLLAPSE INNER MESSAGE!
+                            </div>
                         </div>
                     </div>
-                </div>
-                <hr>
-            <?php endforeach; ?>
+                <?php endforeach; ?>
+            </div>
         </div>
     </div>
 </div>
