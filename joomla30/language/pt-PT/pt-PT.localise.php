f6dab99f26bf062f5648456d630044bda6fb2bf8
Alexander Potashko <fantomhp@gmail.com>
2013-04-30 14:29:09 +0300

    added portuguese

---
+++ joomla30/language/pt-PT/pt-PT.localise.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * @package    Joomla.Language
+ *
+ * @copyright  Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * pt-PT localise class
+ *
+ * @package  Joomla.Site
+ * @since    1.6
+ */
+abstract class Pt_PTLocalise
+{
+	/**
+	 * Returns the potential suffixes for a specific number of items
+	 *
+	 * @param   int  $count  The number of items.
+	 *
+	 * @return  array  An array of potential suffixes.
+	 *
+	 * @since   1.6
+	 */
+	public static function getPluralSuffixes($count)
+	{
+		if ($count == 0)
+		{
+			$return = array('0');
+		}
+		elseif ($count == 1)
+		{
+			$return = array('1');
+		}
+		else
+		{
+			$return = array('MORE');
+		}
+		return $return;
+	}
+
+	/**
+	 * Returns the ignored search words
+	 *
+	 * @return  array  An array of ignored search words.
+	 *
+	 * @since   1.6
+	 */
+	public static function getIgnoredSearchWords()
+	{
+		$search_ignore = array();
+		$search_ignore[] = "e";
+		$search_ignore[] = "de";
+    	$search_ignore[] = "se";
+    	$search_ignore[] = "por";
+    	$search_ignore[] = "nem";
+		$search_ignore[] = "ou";
+		$search_ignore[] = "mas";
+		$search_ignore[] = "em";
+		return $search_ignore;
+	}
+
+	/**
+	 * Returns the lower length limit of search words
+	 *
+	 * @return  integer  The lower length limit of search words.
+	 *
+	 * @since   1.6
+	 */
+	public static function getLowerLimitSearchWord()
+	{
+		return 3;
+	}
+
+	/**
+	 * Returns the upper length limit of search words
+	 *
+	 * @return  integer  The upper length limit of search words.
+	 *
+	 * @since   1.6
+	 */
+	public static function getUpperLimitSearchWord()
+	{
+		return 40;
+	}
+	/**
+	 * Returns the number of chars to display when searching
+	 *
+	 * @return  integer  The number of chars to display when searching.
+	 *
+	 * @since   1.6
+	 */
+	public static function getSearchDisplayedCharactersNumber()
+	{
+		return 200;
+	}
+}
