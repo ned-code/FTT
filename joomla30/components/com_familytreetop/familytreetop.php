5f84debdf28f3ebd8fa022a354edfa43722ae943
alexander.potashko <fantomhp@gmail.com>
2013-06-14 16:00:53 +0400

    update template for footer links

--- joomla30/components/com_familytreetop/familytreetop.php
+++ joomla30/components/com_familytreetop/familytreetop.php
@@ -1,6 +1,6 @@
 <?php
 defined('_JEXEC') or die;
-
+/*
 require_once JPATH_ADMINISTRATOR . DIRECTORY_SEPARATOR . 'components/com_familytreetop/helpers/settings.php';
 require_once JPATH_ADMINISTRATOR . DIRECTORY_SEPARATOR . 'components/com_familytreetop/helpers/activerecord.php';
 require_once JPATH_ADMINISTRATOR . DIRECTORY_SEPARATOR . 'components/com_familytreetop/helpers/facebook.php';
@@ -9,11 +9,13 @@ require_once JPATH_ADMINISTRATOR . DIRECTORY_SEPARATOR . 'components/com_familyt
 require_once JPATH_ADMINISTRATOR . DIRECTORY_SEPARATOR . 'components/com_familytreetop/helpers/upload.php';
 require_once JPATH_ADMINISTRATOR . DIRECTORY_SEPARATOR . 'components/com_familytreetop/helpers/languages.php';
 
+
 ActiverecrdHelper::getInstance();
 FacebookHelper::getInstance();
 FamilyTreeTopSettingsHelper::getInstance();
 
 LanguagesHelper::init();
+*/
 
 $controller = JControllerLegacy::getInstance('Familytreetop');
 $controller->execute(JFactory::getApplication()->input->get('task', 'display'));
