18ffe93450712c9cb999534d1b78845cc1e7c89f
alexander.potashko <fantomhp@gmail.com>
2013-08-14 12:26:28 +0300

    update tree creation. added article

--- joomla30/components/com_familytreetop/views/create/tmpl/default.php
+++ joomla30/components/com_familytreetop/views/create/tmpl/default.php
@@ -2,9 +2,28 @@
 defined('_JEXEC') or die;
 ?>
 <div class="row">
+    <div id="" class="span12">
+        <?php
+            $articleId = 6;
+
+            $db = JFactory::getDbo();
+
+            $sql = "SELECT * FROM #__content WHERE id = ".intval($articleId);
+            $db->setQuery($sql);
+            $fullArticle = $db->loadAssocList('id');
+            $text = $fullArticle[$articleId]['introtext'];
+
+            if(!strlen(trim($text))) $text = "";
+
+            echo $text;
+        ?>
+    </div>
+</div>
+<div class="row">
     <div class="span12 center">
-        <div class="well">
-            <a href="<?=JRoute::_("index.php?option=com_familytreetop&view=create&layout=form", false) ?>" class="btn btn-large btn-primary">Create Tree</a>
+        <div class="well text-center">
+            <a href="<?=JRoute::_("index.php?option=com_familytreetop&view=create&layout=form", false) ?>" class="btn btn-large btn-primary">Continue</a>
+            <a href="<?=JRoute::_("index.php?option=com_familytreetop&view=index", false) ?>" class="btn btn-large">Cancel</a>
         </div>
     </div>
 </div>
