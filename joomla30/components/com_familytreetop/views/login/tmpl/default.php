ebd07a1a5e11f94ae14704424303ee11855eee57
alexander.potashko <fantomhp@gmail.com>
2013-09-19 17:08:40 +0300

    update login template

--- joomla30/components/com_familytreetop/views/login/tmpl/default.php
+++ joomla30/components/com_familytreetop/views/login/tmpl/default.php
@@ -1,28 +1,32 @@
 <?php
 defined('_JEXEC') or die;
 ?>
-<div class="row" id="loginHeader">
-    <div class="span4"></div>
-    <div class="span4 text-center">
-        <img src="<?=$this->baseurl;?>/templates/familytreetop/images/ftt_title.png" accesskey="">
-    </div>
-    <div class="span4"></div>
-</div>
-<div class="row" id="loginContent">
-    <div class="span4"></div>
-    <div class="span4">
-        <div class="well text-center">
-            <a id="login" data-complete-text="Login" data-loading-text="Loading..."  href="#" onclick="return false;" class="btn btn-large">Login</a>
+<div id="loginCont">
+    <div id="loginWrap">
+        <div class="row" id="loginHeader">
+            <div class="span4"></div>
+            <div class="span4 text-center">
+                <img src="<?=$this->baseurl;?>/templates/familytreetop/images/ftt_title.png" accesskey="">
+            </div>
+            <div class="span4"></div>
         </div>
-        <div style="visibility: hidden;" class="progress progress-striped active">
-            <div class="bar" style="width: 0%;"></div>
+        <div class="row" id="loginContent">
+            <div class="span4"></div>
+            <div class="span4">
+                <div class="well text-center">
+                    <a id="login" data-complete-text="Login" data-loading-text="Loading..."  href="#" onclick="return false;" class="btn btn-large">Login</a>
+                </div>
+                <div style="visibility: hidden;" class="progress progress-striped active">
+                    <div class="bar" style="width: 0%;"></div>
+                </div>
+            </div>
+            <div class="span4"></div>
         </div>
     </div>
-    <div class="span4"></div>
-</div>
-<div class="row" id="loginFooter">
-    <div class="span12">
-        <img src="<?=$this->baseurl;?>/templates/familytreetop/images/family_line.png" accesskey="">
+    <div class="row" id="loginFooter">
+        <div class="span12">
+            <img src="<?=$this->baseurl;?>/templates/familytreetop/images/family_line.png" accesskey="">
+        </div>
     </div>
 </div>
 <script>
