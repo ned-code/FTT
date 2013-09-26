f4aa29a75aca6f97e9432ec3924e64fd519a44fe
alexander.potashko <fantomhp@gmail.com>
2013-09-26 14:28:37 +0300

    update familytreetop editor

--- joomla30/templates/familytreetop/js/familytreetop.editor.js
+++ joomla30/templates/familytreetop/js/familytreetop.editor.js
@@ -6,6 +6,7 @@ $FamilyTreeTop.create("editor", function($){
 
     $fn = {
         setOptions: function(parent, ind, callback){
+            /*
             var active = false;
             $(parent).find('[familytreetop-button="delete"]').click(function(){
                 _init_('delete');
@@ -50,6 +51,7 @@ $FamilyTreeTop.create("editor", function($){
                     $(element).click(callback);
                 });
             }
+            */
         },
         setUserMedia: function(parent, ind){
             var dataBox = $('#dataEditMedia').clone(),
