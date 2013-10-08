c27cea7ac954b5c8bccd64241b17d9ceee698aea
alexander.potashko <fantomhp@gmail.com>
2013-10-08 17:59:51 +0300

    update php code myfamily module

--- joomla30/modules/mod_ftt_myfamily/tmpl/default.php
+++ joomla30/modules/mod_ftt_myfamily/tmpl/default.php
@@ -16,38 +16,43 @@ try {
         ));
 
         $members = json_decode($gedcom->getTreeUsers(true, true));
-        $home = $facebook->api('/' . $user->facebook_id . '/home?limit=100');
-        $data = $home['data'];
+        //$home = $facebook->api('/' . $user->facebook_id . '/home?limit=100');
+        //$data = $home['data'];
     } else {
-        $data = null;
+        //$data = null;
     }
 
     $search = array();
+    $string = "";
     foreach($members as $member){
+        $string .= '"'. $member->facebook_id . '",';
         $search[$member->facebook_id] = $gedcom->individuals->get($member->gedcom_id);
     }
 
 
     foreach($family as $member){
+        $string .= '"'. $member['uid'] . '",';
         $search[$member['uid']] = $member;
     }
 
+    /*
     foreach($data as $object){
         $facebook_id = $object['from']['id'];
         if(isset($search[$facebook_id])){
             $result_array[] = $object;
         }
     }
-
+    */
 
     $fb_feed_params = array(
         'method' => 'fql.query',
-        'query' => "SELECT attachment, action_links, post_id, actor_id, target_id, message, description, permalink, likes, created_time, type FROM stream WHERE filter_key = 'nf' AND is_hidden = 0 LIMIT 100",
+        'query' => "SELECT attachment, action_links, post_id, actor_id, target_id, message, description, permalink, likes, created_time, type FROM stream WHERE filter_key = 'nf' AND is_hidden = 0 AND actor_id in (".substr($string, 0, -1).") LIMIT 100",
     );
 
     $fb_news_feed = $facebook->api($fb_feed_params);
 
     //var_dump($fb_news_feed);
+    //var_dump(substr($string, 0, -1));
 
 } catch(Exception $php_errormsg){
 
@@ -71,10 +76,8 @@ try {
 </div>
 <script>
     $FamilyTreeTop.bind(function($){
-        var _json = <?=json_encode(array('search'=>$search, 'sort'=>$data));?>;
+        //var _json = <?=json_encode(array('search'=>$search, 'sort'=>$data));?>;
         var json = <?=json_encode(array('search'=>$search, 'sort'=>$fb_news_feed));?>;
-        console.log(json);
-        console.log(_json);
-        //$FamilyTreeTop.fn.mod('myfamily').render(json);
+        $FamilyTreeTop.fn.mod('myfamily').render(json);
     });
 </script>
