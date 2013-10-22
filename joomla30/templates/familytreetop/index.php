c2090cc7957160376887d75ef0d5e9f527849e30
alexander.potashko <fantomhp@gmail.com>
2013-10-22 16:21:48 +0300

    fix modal

--- joomla30/templates/familytreetop/index.php
+++ joomla30/templates/familytreetop/index.php
@@ -253,7 +253,7 @@ $settings = FamilyTreeTopSettingsHelper::getInstance()->get();
 
     <div id="modal-exist-family-member" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
         <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal-exist-family-member" aria-hidden="true">×</button>
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
             <h3 id="modalLabel"><?=JText::_('TPL_FAMILYTREETOP_MODAL_EXIST_FAMILY_MEMBER_TITLE');?></h3>
         </div>
         <div class="modal-body" style="text-align: center;">
