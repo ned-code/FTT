2c845574a3ad5e1c08a71cf5699c7b3c317f5236
alexander.potashko <fantomhp@gmail.com>
2013-11-04 16:28:17 +0300

    change link in footer - contact

--- joomla30/modules/mod_ftt_footer/tmpl/default.php
+++ joomla30/modules/mod_ftt_footer/tmpl/default.php
@@ -7,6 +7,6 @@ defined('_JEXEC') or die;
         <li><a href="<?=JRoute::_("index.php?option=com_content&view=article&id=2", false);?>">Terms</a></li>
         <li><a href="<?=JRoute::_("index.php?option=com_content&view=article&id=4", false);?>">Privacy</a></li>
         <li><a href="<?=JRoute::_("index.php?option=com_content&view=article&id=5", false);?>">Help</a></li>
-        <li><a href="<?=JRoute::_("index.php?option=com_contact&view=contact&id=1", false);?>">Contact</a></li>
+        <li><a href="<?=JRoute::_("index.php?option=com_flexicontactplus&view=contact", false);?>">Contact</a></li>
     </ul>
 
