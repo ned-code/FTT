e99e772c737713281dcbbade2f3c78c21c3ef035
alexander.potashko <fantomhp@gmail.com>
2013-11-27 17:17:42 +0300

    update myfamily module. added setNull when data is null.

--- joomla30/modules/mod_ftt_myfamily/tmpl/default.php
+++ joomla30/modules/mod_ftt_myfamily/tmpl/default.php
@@ -6,7 +6,7 @@ $user = FamilyTreeTopUserHelper::getInstance()->get();
 $data = array();
 
 if($user->facebook_id != 0){
-   $data = $fhelper->getFacebookNewsFeed($user->tree_id, $user->facebook_id);
+   //$data = $fhelper->getFacebookNewsFeed($user->tree_id, $user->facebook_id);
 }
 ?>
 <div id="myFamilyOnFacebook" class="row-fluid">
@@ -19,6 +19,12 @@ if($user->facebook_id != 0){
                         <div class="span12">
                             <table style="margin:0;" class="table"></table>
                         </div>
+                    </div>
+                <?php else: ?>
+                    <div style="text-align: center;padding: 10px;">
+                        <div style="font-weight: bold;"><?=JText::_('MOD_FTT_MYFAMILY_NULL_TITLE');?></div>
+                        <div><a href="https://facebook.com/<?=$user->username;?>" target="_blank" style="text-decoration: none;"><i class="icon-facebook-sign familytreetop-icon-muted" style="font-size:120px;"></i></a></div>
+                        <div><?=JText::_('MOD_FTT_MYFAMILY_NULL_DESCRIPTION');?></div>
                     </div>
                 <?php endif; ?>
             </fieldset>
