c6e74d934c1b2032d25ee41bd69b7092dcc486df
alexander.potashko <fantomhp@gmail.com>
2013-12-25 17:51:55 +0300

    added superbrowserupdate.js

--- joomla30/templates/familytreetop/index.php
+++ joomla30/templates/familytreetop/index.php
@@ -937,6 +937,7 @@ $treeViewUsers = GedcomHelper::getInstance()->sortUsersByView($treeUsers, $data)
 <jdoc:include type="modules" name="debug" style="none" />
 <!--<script src="<?php echo $this->baseurl ?>/templates/<?=$this->template; ?>/js/jquery-1.9.1.min.js"></script>-->
 <script src="<?php echo $this->baseurl ?>/templates/<?=$this->template; ?>/js/jquery-2.0.3.min.js"></script>
+<script src="<?php echo $this->baseurl ?>/templates/<?=$this->template; ?>/js/superbrowserupdate.js"></script>
 <script src="<?php echo $this->baseurl ?>/templates/<?=$this->template; ?>/js/bootstrap.min.js"></script>
 <script src="<?php echo $this->baseurl ?>/templates/<?=$this->template; ?>/js/bootstrap-scroll-modal.js"></script>
 <script src="<?php echo $this->baseurl ?>/templates/<?=$this->template; ?>/js/tdfriendselector.js"></script>
