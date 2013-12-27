99cf6ef9dc91b7b01a34befa296f373ecf6a0098
alexander.potashko <fantomhp@gmail.com>
2013-12-27 11:16:45 +0300

    update footer

--- joomla30/modules/mod_ftt_footer/tmpl/default.php
+++ joomla30/modules/mod_ftt_footer/tmpl/default.php
@@ -2,7 +2,9 @@
 defined('_JEXEC') or die;
 //About – Terms – Privacy – Help - Contact
 ?>
+    <hr>
     <ul class="unstuled inline">
+        <li><a href="<?=JRoute::_("index.php?option=com_content&view=myfamily", false);?>">Home</a></li>
         <li><a href="<?=JRoute::_("index.php?option=com_content&view=article&id=1", false);?>">About</a></li>
         <li><a href="<?=JRoute::_("index.php?option=com_content&view=article&id=2", false);?>">Terms</a></li>
         <li><a href="<?=JRoute::_("index.php?option=com_content&view=article&id=4", false);?>">Privacy</a></li>
