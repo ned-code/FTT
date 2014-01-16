ccd84231459037b292ba374e588b588be007f61e
alexander.potashko <fantomhp@gmail.com>
2014-01-16 11:19:39 +0300

    iupdate charset to activerecord.

--- joomla30/administrator/components/com_familytreetop/helpers/activerecord.php
+++ joomla30/administrator/components/com_familytreetop/helpers/activerecord.php
@@ -20,7 +20,7 @@ class ActiverecrdHelper
                 $db = $config->get( 'db' );
                 $cfg->set_model_directory(JPATH_ADMINISTRATOR . DIRECTORY_SEPARATOR . '/components/com_familytreetop/php-activerecord/models');
                 $cfg->set_connections(array(
-                    'development' => 'mysql://'.$user.':'.$password.'@'.$host.'/'.$db));
+                    'development' => 'mysql://'.$user.':'.$password.'@'.$host.'/'.$db.'?charset=utf8'));
             });
 
             self::$instance = new ActiverecrdHelper ();
