932d3d63149b9391ce896a82df5af73dbee26a78
alexander.potashko <fantomhp@gmail.com>
2014-02-24 17:09:11 +0300

    update template

--- joomla30/components/com_familytreetop/familytreetop.php
+++ joomla30/components/com_familytreetop/familytreetop.php
@@ -15,6 +15,28 @@ FamilyTreeTopSettingsHelper::getInstance();
 FamilyTreeTopUserHelper::getInstance();
 FamilyTreeTopLanguagesHelper::init();
 
+$doc = JFactory::getDocument();
+$app = & JFactory::getApplication();
+$template = $app->getTemplate();
+
+$doc->addStyleSheet('templates/'.$template.'/css/bootstrap.min.css');
+$doc->addStyleSheet('templates/'.$template.'/css/bootstrap-responsive.min.css');
+$doc->addStyleSheet('templates/'.$template.'/css/bootstrap-combined.no-icons.min.css');
+$doc->addStyleSheet('templates/'.$template.'/css/font-awesome.css');
+//$doc->addStyleSheet('templates/'.$template.'/css/bootstrap.icon-large.min.css');
+$doc->addStyleSheet('templates/'.$template.'/css/bootstrap-scroll-modal.css');
+$doc->addStyleSheet('templates/'.$template.'/css/bootstrap.fix.css');
+$doc->addStyleSheet('templates/'.$template.'/css/jquery.fileupload-ui.css');
+$doc->addStyleSheet('templates/'.$template.'/css/csstreeview.css');
+$doc->addStyleSheet('templates/'.$template.'/css/csstreeview.fix.css');
+$doc->addStyleSheet('templates/'.$template.'/css/tdfriendselector.css');
+$doc->addStyleSheet('templates/'.$template.'/css/familytreetop.css');
+$doc->addStyleSheet('templates/'.$template.'/css/flexicontactplus.fix.css');
+
+$doc->addScript(JURI::base()."/templates/".$template."/js/holder.js");
+$doc->addScript(JURI::base()."/templates/".$template."/js/excanvas.js");
+$doc->addScript(JURI::base()."/templates/".$template."/js/familytreetop.js");
+
 $controller = JControllerLegacy::getInstance('Familytreetop');
 $controller->execute(JFactory::getApplication()->input->get('task', 'display'));
 $controller->redirect();
--- joomla30/templates/familytreetop/index.php
+++ joomla30/templates/familytreetop/index.php
@@ -19,21 +19,6 @@ $params = JFactory::getApplication()->getTemplate(true)->params;
 $user = JFactory::getUser();
 $fttUser = FamilyTreeTopUserHelper::getInstance()->get();
 $app = JFactory::getApplication();
-$doc = JFactory::getDocument();
-
-$doc->addStyleSheet('templates/'.$this->template.'/css/bootstrap.min.css');
-$doc->addStyleSheet('templates/'.$this->template.'/css/bootstrap-responsive.min.css');
-$doc->addStyleSheet('templates/'.$this->template.'/css/bootstrap-combined.no-icons.min.css');
-$doc->addStyleSheet('templates/'.$this->template.'/css/font-awesome.css');
-//$doc->addStyleSheet('templates/'.$this->template.'/css/bootstrap.icon-large.min.css');
-$doc->addStyleSheet('templates/'.$this->template.'/css/bootstrap-scroll-modal.css');
-$doc->addStyleSheet('templates/'.$this->template.'/css/bootstrap.fix.css');
-$doc->addStyleSheet('templates/'.$this->template.'/css/jquery.fileupload-ui.css');
-$doc->addStyleSheet('templates/'.$this->template.'/css/csstreeview.css');
-$doc->addStyleSheet('templates/'.$this->template.'/css/csstreeview.fix.css');
-$doc->addStyleSheet('templates/'.$this->template.'/css/tdfriendselector.css');
-$doc->addStyleSheet('templates/'.$this->template.'/css/familytreetop.css');
-$doc->addStyleSheet('templates/'.$this->template.'/css/flexicontactplus.fix.css');
 
 // Add familytreetop settings
 $settings = FamilyTreeTopSettingsHelper::getInstance()->get();
@@ -51,9 +36,6 @@ $script = "";
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?=$this->template; ?>/css/style.css">
     <noscript><link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?=$this->template; ?>/css/jquery.fileupload-ui-noscript.css"></noscript>
-    <script src="<?php echo $this->baseurl ?>/templates/<?=$this->template; ?>/js/holder.js"></script>
-    <script src="<?php echo $this->baseurl ?>/templates/<?=$this->template; ?>/js/excanvas.js"></script>
-    <script src="<?php echo $this->baseurl ?>/templates/<?=$this->template; ?>/js/familytreetop.js?<?=$script;?>"></script>
 	<jdoc:include type="head" />
 	<!--[if lt IE 9]>
 		<script src="<?php echo $this->baseurl ?>/media/jui/js/html5.js"></script>
@@ -103,5 +85,66 @@ $script = "";
         <jdoc:include type="modules" name="footer" style="" />
     </div>
 </div>
+<!-- The template to display files available for upload -->
+<script id="template-upload" type="text/x-tmpl">
+    {% for (var i=0, file; file=o.files[i]; i++) { %}
+    <tr class="template-upload fade">
+        <td class="preview"><span class="fade"></span></td>
+        <td class="name"><span>{%=file.name%}</span></td>
+        <td class="size"><span>{%=o.formatFileSize(file.size)%}</span></td>
+        {% if (file.error) { %}
+        <td class="error" colspan="2"><span class="label label-important">Error</span> {%=file.error%}</td>
+        {% } else if (o.files.valid && !i) { %}
+        <td>
+            <div class="progress progress-success progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"><div class="bar" style="width:0%;"></div></div>
+        </td>
+        <td>{% if (!o.options.autoUpload) { %}
+            <button class="btn btn-primary start">
+                <i class="icon-upload icon-white"></i>
+                <span>Start</span>
+            </button>
+            {% } %}</td>
+        {% } else { %}
+        <td colspan="2"></td>
+        {% } %}
+        <td>{% if (!i) { %}
+            <button class="btn btn-warning cancel">
+                <i class="icon-ban-circle icon-white"></i>
+                <span>Cancel</span>
+            </button>
+            {% } %}</td>
+    </tr>
+    {% } %}
+</script>
+<!-- The template to display files available for download -->
+<script id="template-download" type="text/x-tmpl">
+    {% for (var i=0, file; file=o.files[i]; i++) { %}
+    <tr class="template-download fade">
+        {% if (file.error) { %}
+        <td></td>
+        <td class="name"><span>{%=file.name%}</span></td>
+        <td class="size"><span>{%=o.formatFileSize(file.size)%}</span></td>
+        <td class="error" colspan="2"><span class="label label-important">Error</span> {%=file.error%}</td>
+        {% } else { %}
+
+        <td class="preview">{% if (file.thumbnail_url) { %}
+            <a href="#{%=file.url%}" title="{%=file.name%}" data-gallery="gallery" download="{%=file.name%}"><img src="#{%=file.thumbnail_url%}"></a>
+            {% } %}</td>
+        <td class="name">
+            <a href="#{%=file.url%}" title="{%=file.name%}" data-gallery="{%=file.thumbnail_url&&'gallery'%}" download="{%=file.name%}">{%=file.original_name%}</a>
+        </td>
+        <td class="size"><span>{%=o.formatFileSize(file.size)%}</span></td>
+        <td colspan="2"></td>
+        {% } %}
+        <td>
+            <button class="btn btn-danger delete" data-type="{%=file.delete_type%}" data-url="{%=file.delete_url%}"{% if (file.delete_with_credentials) { %} data-xhr-fields='{"withCredentials":true}'{% } %}>
+            <i class="icon-trash icon-white"></i>
+            <span>Delete</span>
+            </button>
+            <input type="checkbox" name="delete" value="1" class="toggle">
+        </td>
+    </tr>
+    {% } %}
+</script>
 </body>
 </html>
