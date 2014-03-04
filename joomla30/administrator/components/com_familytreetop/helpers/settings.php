6d6ca292c386800856d00b6e35a1ea63e74c773b
alexander.potashko <fantomhp@gmail.com>
2014-03-04 19:05:57 +0300

    update settings init helper.

--- joomla30/administrator/components/com_familytreetop/helpers/settings.php
+++ joomla30/administrator/components/com_familytreetop/helpers/settings.php
@@ -34,9 +34,7 @@ class FamilyTreeTopSettingsHelper
             $args->params = $object->params;
             $settings->{$object->name} = $args;
         }
-
-
-        $settings->{'SERVER_NAME'} = $_SERVER['SERVER_NAME'];
+        $settings->{'SERVER_NAME'} = self::$instance->getServerName();
         $settings->_template = $settings->{$settings->SERVER_NAME . ".template"};
         self::$instance->settings = $settings;
     }
