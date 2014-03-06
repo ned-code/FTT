a5d02d73a4780cad20813b8413393efec638018e
alexander.potashko <fantomhp@gmail.com>
2014-03-06 15:07:40 +0300

    update footer module.

--- joomla30/modules/mod_ftt_footer/tmpl/default.php
+++ joomla30/modules/mod_ftt_footer/tmpl/default.php
@@ -1,7 +1,10 @@
 <?php
 defined('_JEXEC') or die;
 //About – Terms – Privacy – Help - Contact
+$settings = FamilyTreeTopSettingsHelper::getInstance()->get();
+$template = $settings->_template->value;
 ?>
+<?php if($template == "familytreetop"): ?>
     <hr>
     <ul class="unstuled inline">
         <li><a href="<?=JRoute::_("index.php?option=com_familytreetop&view=myfamily", false);?>">Home</a></li>
@@ -11,4 +14,15 @@ defined('_JEXEC') or die;
         <li><a href="<?=JRoute::_("index.php?option=com_content&view=article&id=5", false);?>">Help</a></li>
         <li><a href="<?=JRoute::_("index.php?option=com_flexicontactplus&view=contact", false);?>">Contact</a></li>
     </ul>
+<?php else: ?>
+    <hr>
+    <ul class="unstuled inline">
+        <li><a href="<?=JRoute::_("index.php?option=com_familytreetop&view=myfamily", false);?>">Home</a></li>
+        <li><a href="<?=JRoute::_("index.php?option=com_content&view=article&id=10", false);?>">About</a></li>
+        <li><a href="<?=JRoute::_("index.php?option=com_content&view=article&id=11", false);?>">Terms</a></li>
+        <li><a href="<?=JRoute::_("index.php?option=com_content&view=article&id=14", false);?>">Privacy</a></li>
+        <li><a href="<?=JRoute::_("index.php?option=com_content&view=article&id=12", false);?>">Help</a></li>
+        <li><a href="<?=JRoute::_("index.php?option=com_flexicontactplus&view=contact", false);?>">Contact</a></li>
+    </ul>
+<?php endif; ?>
 
