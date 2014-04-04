a20b62b402ab26423197b079803632e22df07475
alexander.potashko <fantomhp@gmail.com>
2014-04-04 11:53:32 +0300

    update latest events

--- joomla30/components/com_familytreetop/js/familytreetop.latest.events.js
+++ joomla30/components/com_familytreetop/js/familytreetop.latest.events.js
@@ -15,7 +15,7 @@ $FamilyTreeTop.create("latest_events", function($){
             return img;
         },
         setNull: function(box){
-            $(box).append('<tr familytreetop="null" ><td colspan="3" style="text-align:center; padding: 20px;"><i class="icon-calendar-empty muted" style="font-size:28px;color: #acacac;"></i> <span style="color: #959595;">'+$languages['none']+'</span></td></tr>')
+            $(box).append('<tr familytreetop="null" ><td colspan="3" style="text-align:center; padding: 20px;"><i class="fa fa-calendar-o muted" style="font-size:28px;color: #acacac;"></i> <span style="color: #959595;">'+$languages['none']+'</span></td></tr>')
         },
         setEvents: function(box, events, type){
             var user, family, husb, wife, div, ev, tr, td, avatar;
@@ -46,9 +46,9 @@ $FamilyTreeTop.create("latest_events", function($){
                     ev = family.event();
                     $(tr).addClass('familytreetop-hover-effect');
                     $(tr).attr('gedcom_id', 'family:' + husb.gedcom_id + "," + wife.gedcom_id);
-                    $(tr).append('<td class="text-center" style="padding-left:10px;width:100px; vertical-align: middle;">'+$this.mod('usertree').parseDate(ev.date)+'</td>');
+                    $(tr).append('<td class="text-center" style="padding-left:10px;width:120px; vertical-align: middle;">'+$this.mod('usertree').parseDate(ev.date)+'</td>');
                     $(tr).append(td);
-                    $(tr).append('<td><div gedcom_id="'+husb.gedcom_id+'"><div data-familytreetop-color="'+husb.gender+'">'+husb.name()+'</div><div class="familytreetop-this-month-relation"><i class="icon-leaf"></i>'+husb.relation+'</div></div><div gedcom_id="'+wife.gedcom_id+'"><div style="margin-top: 5px;" data-familytreetop-color="'+wife.gender+'">'+wife.name()+'</div><div class="familytreetop-this-month-relation"><i class="icon-leaf"></i>'+wife.relation+'</div></div></td>');
+                    $(tr).append('<td><div gedcom_id="'+husb.gedcom_id+'"><div data-familytreetop-color="'+husb.gender+'">'+husb.name()+'</div><div class="familytreetop-this-month-relation"><i class="fa fa-leaf"></i>'+husb.relation+'</div></div><div gedcom_id="'+wife.gedcom_id+'"><div style="margin-top: 5px;" data-familytreetop-color="'+wife.gender+'">'+wife.name()+'</div><div class="familytreetop-this-month-relation"><i class="fa fa-leaf"></i>'+wife.relation+'</div></div></td>');
                     $(tr).find('div[gedcom_id]').each(function(i, el){
                         $this.mod('popovers').render({
                             target: el,
@@ -64,9 +64,9 @@ $FamilyTreeTop.create("latest_events", function($){
                     $(tr).addClass('familytreetop-hover-effect');
                     //$(tr).attr('gedcom_id', user.gedcom_id);
                     $(td).append(avatar);
-                    $(tr).append('<td class="text-center" style="padding-left:10px;width:100px; vertical-align: middle;">'+$this.mod('usertree').parseDate(ev.date)+'</td>');
+                    $(tr).append('<td class="text-center" style="padding-left:10px;width:120px; vertical-align: middle;">'+$this.mod('usertree').parseDate(ev.date)+'</td>');
                     $(tr).append(td);
-                    $(tr).append('<td><div div="'+user.gedcom_id+'" data-familytreetop-color="'+user.gender+'" gedcom_id="'+user.gedcom_id+'">'+user.name()+'</div><div class="familytreetop-this-month-relation"><i class="icon-leaf"></i>'+user.relation+'</div></td>');
+                    $(tr).append('<td><div div="'+user.gedcom_id+'" data-familytreetop-color="'+user.gender+'" gedcom_id="'+user.gedcom_id+'">'+user.name()+'</div><div class="familytreetop-this-month-relation"><i class="fa fa-leaf"></i>'+user.relation+'</div></td>');
                     $this.mod('popovers').render({
                         target: $(tr).find('div[gedcom_id]'),
                         placement: "left"
